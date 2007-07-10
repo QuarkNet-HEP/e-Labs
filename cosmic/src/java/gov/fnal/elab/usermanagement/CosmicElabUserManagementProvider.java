fe794fcda527de778f936c768b4a85742d2ae13b
hategan <mhategan@gmail.com>
2007-07-10 18:48:41 +0000

    sync

---
+++ cosmic/src/java/gov/fnal/elab/usermanagement/CosmicElabUserManagementProvider.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Jun 28, 2007
+ */
+package gov.fnal.elab.usermanagement;
+
+import gov.fnal.elab.ElabGroup;
+import gov.fnal.elab.util.ElabException;
+
+import java.util.Collection;
+
+public interface CosmicElabUserManagementProvider extends
+        ElabUserManagementProvider {
+
+    Collection getDetectorIds(ElabGroup group) throws ElabException;
+    
+    void setDetectorIds(ElabGroup group, Collection detectorIds) throws ElabException;
+}
