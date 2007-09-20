0e4432db3661ca586fb4ac9cfa988198ec7b285c
hategan <mhategan@gmail.com>
2007-09-20 23:13:36 +0000

    new lines between data files

--- common/src/java/gov/fnal/elab/datacatalog/DataTools.java
+++ common/src/java/gov/fnal/elab/datacatalog/DataTools.java
@@ -206,7 +206,7 @@ public class DataTools {
                     dataCount++;
                 }
                 if (i.hasNext()) {
-                    data.append(", ");
+                    data.append("\n");
                 }
             }
             else if (dataCount != Integer.MAX_VALUE) {
