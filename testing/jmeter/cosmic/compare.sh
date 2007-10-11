cac5d086342a183f3c643569f7ef77424284f4fc
hategan <mhategan@gmail.com>
2007-10-11 20:24:18 +0000

    updated tests

--- testing/jmeter/cosmic/compare.sh
+++ testing/jmeter/cosmic/compare.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+sleep 2
 TYPE=$1
 REF="reference-$TYPE.png"
 URL=$2
-
-wget -r -O output-$TYPE.png $URL
+LOG=compare-$TYPE.log
+wget -o $LOG -r -O output-$TYPE.png $URL 
+echo "URL: $URL" >>$LOG
 
 DIFF=`compare -metric MAE reference-$TYPE.png output-$TYPE.png null: 2>&1`
+
+echo "DIFF: $DIFF">>$LOG
 
 if [ "$DIFF" -lt "20" ]; then
 	echo "good enough"
