aea35894eadcbbdf2cf8ae45f8d28ccbfd9b44b0
hategan <mhategan@gmail.com>
2007-10-11 00:11:36 +0000

    more persistent analysis manager

--- common/src/java/gov/fnal/elab/analysis/AnalysisManager.java
+++ common/src/java/gov/fnal/elab/analysis/AnalysisManager.java
@@ -3,26 +3,46 @@
  */
 package gov.fnal.elab.analysis;
 
+import gov.fnal.elab.Elab;
+import gov.fnal.elab.ElabGroup;
+
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import javax.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
 
 /**
  * This class is used to manage a set of analyses in a session
  */
 public class AnalysisManager {
-    public static final String SESSION_ANALYSES = "elab:analyses";
+    public static final Logger logger = Logger.getLogger(AnalysisManager.class);
+    
+    /**
+     * The interval, in milliseconds at which the reaper wakes and reaps
+     * analyses
+     */
+    public static final int REAPER_WAKE_INTERVAL = 60 * 60 * 1000;
+
+    public static final String ANALYSES = "elab:analyses";
+    public static final String RUN_ATTR_START_TIME = "startTime";
+
+    private static Map reapers = new HashMap();
 
     /**
      * Registers an analysis run in the specified session
      */
-    public static void registerAnalysisRun(HttpSession session, AnalysisRun run) {
-        Map a = getAnalysisRuns(session);
+    public static void registerAnalysisRun(Elab elab, ElabGroup user,
+            AnalysisRun run) {
+        Map a = getAnalysisRuns(elab, user);
         synchronized (a) {
             a.put(run.getId(), run);
+            run.setAttribute(RUN_ATTR_START_TIME, new Date());
         }
     }
 
@@ -30,24 +50,45 @@ public class AnalysisManager {
      * Returns a {id -> run} map of analysis runs registered with the specified
      * session.
      */
-    public static Map getAnalysisRuns(HttpSession session) {
+    public static Map getAnalysisRuns(Elab elab, ElabGroup user) {
         Map a;
-        synchronized (session) {
-            a = (Map) session.getAttribute(SESSION_ANALYSES);
+        synchronized (AnalysisManager.class) {
+            Map users = getAnalysisRuns(elab);
+            a = (Map) users.get(user.getName());
             if (a == null) {
                 a = new TreeMap();
+                users.put(user.getName(), a);
             }
-            session.setAttribute(SESSION_ANALYSES, a);
         }
         return a;
+    }
+
+    private static Map getAnalysisRuns(Elab elab) {
+        synchronized (AnalysisManager.class) {
+            Map users = (Map) elab.getAttribute(ANALYSES);
+            if (users == null) {
+                users = new HashMap();
+                elab.setAttribute(ANALYSES, users);
+                initializeReaper(elab);
+            }
+            return users;
+        }
+    }
+
+    private static void initializeReaper(Elab elab) {
+        AnalysisReaper reaper = (AnalysisReaper) reapers.get(elab.getName());
+        if (reaper == null) {
+            reaper = new AnalysisReaper(elab);
+            reaper.start();
+        }
     }
 
     /**
      * Returns a collection of IDs of analyses registered in the specified
      * session
      */
-    public static Collection getAnalysisRunIDs(HttpSession session) {
-        Map a = getAnalysisRuns(session);
+    public static Collection getAnalysisRunIDs(Elab elab, ElabGroup user) {
+        Map a = getAnalysisRuns(elab, user);
         synchronized (a) {
             return new ArrayList(a.keySet());
         }
@@ -56,8 +97,9 @@ public class AnalysisManager {
     /**
      * Retrieves an analysis run registered in a session based on its ID
      */
-    public static AnalysisRun getAnalysisRun(HttpSession session, String id) {
-        Map a = getAnalysisRuns(session);
+    public static AnalysisRun getAnalysisRun(Elab elab, ElabGroup user,
+            String id) {
+        Map a = getAnalysisRuns(elab, user);
         synchronized (a) {
             return (AnalysisRun) a.get(id);
         }
@@ -66,10 +108,79 @@ public class AnalysisManager {
     /**
      * Removes an analysis run from a session
      */
-    public static void removeAnalysisRun(HttpSession session, String id) {
-        Map a = getAnalysisRuns(session);
+    public static void removeAnalysisRun(Elab elab, ElabGroup user, String id) {
+        Map a = getAnalysisRuns(elab, user);
         synchronized (a) {
             a.remove(id);
+        }
+    }
+    
+    private static class AnalysisReaper extends Thread {
+        private final Elab elab;
+        private long lifetime;
+
+        public AnalysisReaper(Elab elab) {
+            super("Analysis Reaper: " + elab.getName());
+            setDaemon(true);
+            this.elab = elab;
+            String hr = elab.getProperty("max.analysis.lifetime");
+            try {
+                lifetime = Integer.parseInt(hr);
+            }
+            catch (Exception e) {
+                lifetime = 1;
+                logger.warn("Invalid max.analysis.lifetime: " + hr
+                        + ". Using default: " + lifetime + " hr");
+            }
+            lifetime *= 3600*1000;
+        }
+
+        public void run() {
+            try {
+                while (true) {
+                    Thread.sleep(REAPER_WAKE_INTERVAL);
+                    Map runs = getAnalysisRuns(elab);
+                    Map copy;
+                    synchronized (AnalysisManager.class) {
+                        copy = new HashMap(runs);
+                    }
+                    Iterator i = copy.entrySet().iterator();
+                    while (i.hasNext()) {
+                        Map.Entry e = (Map.Entry) i.next();
+                        Map analyses = (Map) e.getValue();
+                        Collection r = reap(analyses);
+                        synchronized(analyses) {
+                            analyses.keySet().removeAll(r);
+                        }
+                    }
+                }
+            }
+            catch (InterruptedException e) {
+            }
+        }
+
+        private Collection reap(Map analyses) {
+            List l = new ArrayList();
+            if (analyses != null) {
+                Date now = new Date();
+                Iterator i = analyses.entrySet().iterator();
+                while (i.hasNext()) {
+                    Map.Entry e = (Map.Entry) i.next();
+                    String id = (String) e.getKey();
+                    AnalysisRun run = (AnalysisRun) e.getValue();
+                    Date started = (Date) run.getAttribute(RUN_ATTR_START_TIME);
+                    if (started == null) {
+                        logger.warn("Missing start time for run " + id);
+                    }
+                    else {
+                        if (started.getTime() + lifetime < now.getTime()) {
+                            logger.info("Reapping run " + id);
+                            l.add(id);
+                        }
+                    }
+                }
+            }
+            return l;
         }
     }
 }
