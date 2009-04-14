93dfa86e7563c61772e1a901738d6446b0098170
hategan <mhategan@gmail.com>
2009-04-14 19:22:51 +0000

    more resilience to broken logs

--- common/src/java/gov/fnal/elab/statistics/AnalysisStats.java
+++ common/src/java/gov/fnal/elab/statistics/AnalysisStats.java
@@ -82,6 +82,11 @@ public class AnalysisStats {
             while (line != null) {
                 String[] e = line.split("\\s");
                 Date d;
+                if (e.length < 2) {
+                    System.out.println("Invalid line (missing timestamp): " + line);
+                    line = br.readLine();
+                    continue;
+                }
                 try {
                     d = DF.parse(e[1]);
                 }
