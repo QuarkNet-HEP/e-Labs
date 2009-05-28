75e4ccda0b5519c526ee7f6055feb298c42b4d91
myers <>
2009-05-28 15:30:19 +0000

    Remove database access info to a separate file.

--- boinc/html/ops/list_table.php
+++ boinc/html/ops/list_table.php
@@ -6,39 +6,33 @@
  * @(#) $Id:$
 \***********************************************************************/
 
-// Edit here to change query:
-
-$query = "SELECT * FROM research_group"; 
-$query = "SELECT * FROM research_group WHERE role='user' ";
-$query = "SELECT * FROM school";
-$query = "SELECT * FROM teacher ORDER BY name";
-$query = "SELECT * FROM research_group WHERE role='teacher' ORDER BY teacher_id";
+require_once("../ops/eLabDatabase.php");
 
 
-// Production:
-//
-$db_host="data1.i2u2.org";
-$db_name="userdb2006_1022";
-$db_user="portal2006_1022";
-$db_pass="elab";
+$table = $_GET['table'];
+if( empty($table) ) $table = "teacher";
+ 
+$sort = $_GET['sort'];
+if( empty($sort) ) $sort = "name";
 
-// Development and testing:
-//
-$db_host="data1.i2u2.org";
-$db_name="userdb_cosmic2_testing";
-$db_user="portal2006_1022";
-$db_pass="portal2006_1022";
-
+$query = "SELECT * FROM $table ORDER BY $sort, id DESC";
 
 $conn_string="host=$db_host dbname=$db_name user=$db_user password=$db_pass";
 $db = pg_pconnect("$conn_string") 
 	or die("! Failed to connect to database $db_name as user $db_user");
 
 
-echo "<p>Connected to database $db_name as user $db_user <br>\n";
+ echo "<p>Connected to database $db_name (as user $db_user) <br>\n";
 
-$result = pg_query($query)
-	    or die("Query failed: $query");
+echo "<h2><tt>psql> $query</tt></h2>\n";
+
+$result = pg_query($query);
+if( $result===FALSE ){
+      echo "<font color=RED>! Query failed. <br>\n";
+      echo "<tt>" .pg_last_error(). "</tt></font><br>\n";
+      die;
+}
+
 
 // get headers
 
@@ -46,11 +40,11 @@ $row = pg_fetch_assoc($result);
 
 // Printing results in HTML
 
-echo "<h2><tt>$query</tt></h2>\n";
 
-echo "<table>\n";
+
+echo "<table cellpadding=3>\n";
 $n=0;
-
+$prev_value='none';
 
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
   echo "\t<tr>\n";
@@ -62,7 +56,14 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "\t</tr>\n";
   }
   foreach ($line as $col_name => $col_value) {
-    echo "\t\t<td>$col_value</td>\n";
+    $x1=''; $x2='';
+    if( $col_name == $sort ){
+      if( $col_value == $prev_value ){
+	$x1="<font color=RED>"; $x2="</font>";
+      }
+      $prev_value = $col_value;
+    }
+    echo "\t\t<td>$x1 $col_value $x2</td>\n";
   }
   echo "\t</tr>\n";
  }
