42c8298e89a08344f64eb7cd6dae90e337a5e722
myers <>
2009-05-28 15:15:09 +0000

    * remove columns before adding them, in case old ones exist. * remove constraint that authenticator be unique * change file name

--- cosmic/src/sql/add_authenticator.sql
+++ cosmic/src/sql/add_authenticator.sql
@@ -12,10 +12,24 @@
 -- Eric Myers <myers@spy-hill.net>  - 22 May 2009
 
 
+-- The "authenticator" is a 32 character hex number which is unique
+-- to an individual and is a secret used for authentication.  
+-- Ideally this column should be UNIQUE, but we have multiple teachers
+-- with the same e-mail address in the existing database, so until
+-- we cull duplicates do not make it UNIQUE.
+
+ALTER TABLE ONLY teacher
+  DROP COLUMN authenticator;
+ALTER TABLE ONLY teacher
+  ADD COLUMN authenticator character varying(254); 
+
+-- This is the user ID in the forums.  We save it for future use should
+-- we want to be able to index into that database directly.
+
+ALTER TABLE ONLY teacher
+  DROP COLUMN forum_id;
 ALTER TABLE ONLY teacher
   ADD COLUMN forum_id integer;
 
-ALTER TABLE ONLY teacher
-  ADD COLUMN authenticator character varying(254) UNIQUE;
+-- EOF 
 
--- EOF
