b787bf5d056df2e57c1ca800ce2684d1f61410c0
myers <>
2009-05-29 15:24:31 +0000

    Get $next_url using get_destination() utility

--- boinc/html/user/login_form.php
+++ boinc/html/user/login_form.php
@@ -2,13 +2,16 @@
     require_once("../inc/db.inc");
     require_once("../inc/util.inc");
 
-    $next_url = $_GET["next_url"];
-    
+    require_once("../include/util.php"); // additional useful stuff
+
+
     db_init();
     $user = get_logged_in_user(false);
 
+    //$next_url = $_GET["next_url"];
+    $next_url = get_destination();
+
     page_head("Log in/out");
     print_login_form_aux($next_url, $user);
-
     page_tail();
 ?>
