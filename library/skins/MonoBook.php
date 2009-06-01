2e526f3e800a8079566d3772513c04bd82077806
myers <>
2009-06-01 16:50:56 +0000

    Logo links to top of the site, not to Main wiki page.

--- library/skins/MonoBook.php
+++ library/skins/MonoBook.php
@@ -145,7 +145,7 @@ class MonoBookTemplate extends QuickTemplate {
 	</div>
 	<div class="portlet" id="p-logo">
 		<a style="background-image: url(<?php $this->text('logopath') ?>);" <?php
-			?>href="<?php echo htmlspecialchars("/")?>"<?php
+			?>href="<?php echo htmlspecialchars($this->data['nav_urls']['mainpage']['href'])?>"<?php
 			echo $skin->tooltipAndAccesskey('n-mainpage') ?>></a>
 	</div>
 	<script type="<?php $this->text('jsmimetype') ?>"> if (window.isMSIE55) fixalpha(); </script>
