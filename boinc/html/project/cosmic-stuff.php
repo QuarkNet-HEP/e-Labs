0f9a7deae6c97fdca137f398a2b14672a75c32d6
myers <>
2009-06-03 14:56:24 +0000

    Banner functions return text as value so can be used also in wiki or elsewhere.

--- boinc/html/project/cosmic-stuff.php
+++ boinc/html/project/cosmic-stuff.php
@@ -7,63 +7,59 @@
 \**********************************************************************/
 
 function cosmic_masthead($title='',$right_stuff='&nbsp;'){
+  echo "\n<!-- BEGIN Cosmic Ray e-lab masthead -->\n<div class='top'>\n";
+  echo cosmic_banner($title,$right_stuff);
+  echo cosmic_menu_bar();
+  echo "\n</div><!-- END e-lab masthead -->\n";
+}
 
+
+function cosmic_banner($title='',$right_stuff='&nbsp;'){// returns $x
   // TODO: make this smarter - teacher or student?
   $home_link="/elab/cosmic/teacher/";
   $elab_logo="/elab/cosmic/graphics/blast.jpg";
 
-  echo "\n<!-- BEGIN Cosmic Ray e-lab masthead -->\n<div class='top'>\n".
-     "<TABLE class='masthead' width='100%' bgcolor='black' ><tr>
+  $x = "<TABLE class='masthead' width='100%' bgcolor='black' ><tr>
        <TD class='noborder' width='15%' valign='TOP' align='CENTER'>
               <a href='$home_link' >
 	      <img src='$elab_logo'
                    title='return to the e-Lab home'
                    alt='' /></a>
-       </td>\n";
-    echo "
+       </td>
        <TD class='noborder' width='60%' align='LEFT' >
 	<div class='header-title'>
 		Cosmic Ray e-Lab
 	</div>
-<div class='header-subtitle'>
-	    
-        </div>
        </td>\n ";
-
-    echo "
+   $x .= "
        <TD class='noborder' width='25%' valign='TOP' align='RIGHT'>\n";
-       if( isset($hide_user) && $hide_user ) {   // don't show user/login or cache indicator
-	 echo "&nbsp;";
+
+       if( 1 || isset($hide_user) && $hide_user ) {   // don't show user/login or cache indicator
+	 $x .= "&nbsp;";
        }
        else {      
         // need to see if the person is authenticated, cookie or not
         $authenticator = init_session();
-        echo "<font size='1' color='white'>";
+        $x .= "<font size='1' color='white'>";
         $logged_in_user = get_logged_in_user(false);
-        show_login_name($logged_in_user);
-        echo "</font>\n";
+        $x .= show_login_name($logged_in_user);
+        $x .=  "</font>\n";
     }
-    echo "\n</TD></TR>\n";
-
-    echo "</TABLE>";
-
-    //project_menu_bar();
-    cosmic_menu_bar();
-
-    echo "\n</div><!-- END e-lab masthead -->\n";
-  }
-
+    $x .=  "\n</TD></TR>\n";
+    $x .=  "</TABLE>";
+    return $x;
+}
 
 
 
 function cosmic_menu_bar(){
   //TODO: detect teacher -vs- student URL and change accordingly
-  cosmic_teacher_menu_bar();
+  return cosmic_teacher_menu_bar();
 }
 
 
 function cosmic_teacher_menu_bar(){
-    echo "
+    $x = "
     <!-- Teacher's menu bar for Cosmic Ray e-Lab -->
     <TABLE class='menu_bar'><TR>
       <TD>&nbsp;</TD>
@@ -71,59 +67,35 @@ function cosmic_teacher_menu_bar(){
             Teacher Home</a>
       </td>";
 
-    echo "
+    $x .= "
       <TD><a href='/elab/cosmic/teacher/community.jsp'>
             Community</a>
       </td>";
 
-    echo "
+    $x .= "
       <TD><a href='/elab/cosmic/teacher/standards.jsp'>
             Standards</a>
       </td>";
 
-    echo "
+    $x .= "
       <TD><a href='/elab/cosmic/teacher/site-map.jsp'>
             Site Index</a>
       </td>";
 
-    echo "
+    $x .= "
       <TD><a href='/elab/cosmic/teacher/registration.jsp'>
             Registration</a>
       </td>";
 
-    echo "
+    $x .= "
       <TD><a href='/elab/cosmic/home/'>
             Student Home</a>
       </td>";
 
-    echo "\n    </TR>
+    $x .= "\n    </TR>
     </TABLE><!-- END menu bar -->\n";
-}
 
-
-
-
-// Previous Attempt... 
-
-function old_cosmic_masthead($title=''){
-  echo "
-	<TABLE width=100% border=0><TR>\n";
- 
-  echo "<TD id='header-image'>
-	<img src='/elab/cosmic/graphics/blast.jpg' alt='Cosmic Ray Blast' />
-	</TD>\n";
- 
-  echo "<TD valign='top'>
-	<id='header-title'> Cosmic Ray e-Lab</div>\n";
-
-  $nav_file="/home/i2u2/cosmic/src/jsp/include/nav-teacher.jsp";
-  include($nav_file);
-
-echo "</TD>
-    </TR></TABLE>
-	\n";    
-
-  echo "<hr>\n\n";
+    return $x;
 }
 
 ?>
