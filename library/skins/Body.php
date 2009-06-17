c0801ca2b59924518ebe36cbe17f1480f94f2139
myers <>
2009-06-17 20:42:51 +0000

    Trim down what this skin shows even further, turning off every feature we don't need (that we can, so far).

--- library/skins/Body.php
+++ library/skins/Body.php
@@ -12,48 +12,14 @@ if( !defined( 'MEDIAWIKI' ) )
 	die( -1 );
 
 /**
- * If this skin is loaded then set these bare defaults for user options 
- * TODO: move this into initialization
- */
-
-$wgDefaultUserOptions = array( 
-	'quickbar' 		=> 0,
-	'underline' 		=> 2,
-	'skin' 			=> false,
-	'math' 			=> 1,
-	'highlightbroken'	=> 0,
-	'stubthreshold' 	=> 0,
-	'previewontop' 		=> 0,
-	'editsection'		=> 0,
-	'editsectiononrightclick'=> 0,
-	'showtoc'		=> 0,
-	'showtoolbar' 		=> 0,
-	'date' 			=> 'default',
-	'imagesize' 		=> 2,
-	'thumbsize'		=> 2,
-	'rememberpassword' 	=> 0,
-	'enotifwatchlistpages' 	=> 0,
-	'enotifusertalkpages' 	=> 0,
-	'enotifminoredits' 	=> 0,
-	'enotifrevealaddr' 	=> 0,
-	'shownumberswatching' 	=> 0,
-	'fancysig' 		=> 0,
-	'externaleditor' 	=> 0,
-	'externaldiff' 		=> 0,
-	'showjumplinks'		=> 0,
-	'numberheadings'	=> 0,
-	'uselivepreview'	=> 0,
-);
-
-
-/** */
-require_once('includes/SkinTemplate.php');
-
-/**
  * Inherit main code from SkinTemplate, set the CSS and template filter.
  * @todo document
  * @addtogroup Skins
  */
+
+require_once('includes/SkinTemplate.php');
+
+
 class SkinBody extends SkinTemplate {
   function initPage( &$out ) {
     SkinTemplate::initPage( $out );
@@ -71,6 +37,36 @@ class SkinBody extends SkinTemplate {
     global $wgSkipSkins;
     $wgSkipSkins = array("monobook", "cologneblue", "myskin", "simple",
 			 "nostalgia", "standard", "chick");
+
+    global  $wgDefaultUserOptions;
+    $wgDefaultUserOptions = array( 
+       'quickbar'              => 0,
+       'underline'             => 2,
+       'skin'                  => false,
+       'math'                  => 1,
+       'highlightbroken'       => 0,
+       'stubthreshold'         => 0,
+       'previewontop'          => 0,
+       'editsection'           => 0,
+       'editsectiononrightclick'=> 0,
+       'showtoc'               => 0,
+       'showtoolbar'           => 0,
+       'date'                  => 'default',
+       'imagesize'             => 2,
+       'thumbsize'             => 2,
+       'rememberpassword'      => 0,
+       'enotifwatchlistpages'  => 0,
+       'enotifusertalkpages'   => 0,
+       'enotifminoredits'      => 0,
+       'enotifrevealaddr'      => 0,
+       'shownumberswatching'   => 0,
+       'fancysig'              => 0,
+       'externaleditor'        => 0,
+       'externaldiff'          => 0,
+       'showjumplinks'         => 0,
+       'numberheadings'        => 0,
+       'uselivepreview'        => 0,
+				   );
   }
 }
 
@@ -80,48 +76,44 @@ class SkinBody extends SkinTemplate {
  * @addtogroup Skins
  */
 class BodyTemplate extends QuickTemplate {
-	/**
-	 * Template filter callback for Body skin.
-	 * Takes an associative array of data set from a SkinTemplate-based
-	 * class, and a wrapper for MediaWiki's localization database, and
-	 * outputs a formatted page.
-	 *
-	 * @access private
-	 */
-	function execute() {
-		global $wgUser;
-		$skin = $wgUser->getSkin();
 
-		// Suppress warnings to prevent notices about missing indexes in $this->data
-		wfSuppressWarnings();
-		print Skin::makeGlobalVariablesScript( $this->data ); 
-		if($this->data['pagecss'   ]) { ?>
-		<style type="text/css"><?php $this->html('pagecss'   ) ?></style>
-<?php	} ?>
-<div id="globalWrapper">
-     <div id="column-content">
-	<div id="content">
-		<a name="top" id="top"></a>
-		<?php if($this->data['sitenotice']) { ?><div id="siteNotice"><?php $this->html('sitenotice') ?></div><?php } ?>
-		<h1 class="firstHeading"><?php $this->data['displaytitle']!=""?$this->html('title'):$this->text('title') ?></h1>
-		<div id="bodyContent">
-			<h3 id="siteSub"><?php $this->msg('tagline') ?></h3>
-			<div id="contentSub"><?php $this->html('subtitle') ?></div>
+  /**
+   * @access private
+   */
+  function execute() {
+    # Suppress warnings to prevent notices about missing indexes in $this->data
+    wfSuppressWarnings();
 
-			<!-- start content -->
-			<style type="text/css">
-			.editsection {	display: none; }
-			</style>
-			<?php $this->html('bodytext') ?>
-			<?php if($this->data['catlinks']) { ?><div id="catlinks"><?php       $this->html('catlinks') ?></div><?php } ?>
-			<!-- end content -->
+    echo "<!-- Article: ";
+    $this->html('title');
+    echo " --> \n";
 
-		</div>
+    echo "
+    <div id='globalWrapper'>
+      <div id='column-content'>
+        <div id='content'> 
+          <a name='top' id='top'></a>
+          <div id='bodyContent'>\n";
+    echo "\n<!-- start content -->";
+    
+    // this is a kludge to hide editing links and footer until I
+    // figure out how to turn them off completely. -EAM 17Jun2009
+    echo "
+	 <style type='text/css'>
+	    .editsection {	display: none; }
+	    .printfooter {	display: none; }
+	 </style>\n";
+
+    $this->html('bodytext');
+
+    echo "\n<!-- end content -->\n
+	  </div>
 	</div>
-     </div>
-</div>
-<?php
-	wfRestoreWarnings();
-	} // end of execute() method
+      </div>
+    </div>\n \n";
+
+    wfRestoreWarnings();
+  } // end of execute() method
 } // end of class
+
 ?>
