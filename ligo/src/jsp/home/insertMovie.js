5636ff3ace76ec122183575f3abe38f4c3a3e2f2
phongn <phongn@gmail.com>
2009-07-30 21:10:35 +0000

    Fix insertMovie JS

--- ligo/src/jsp/home/insertMovie.js
+++ ligo/src/jsp/home/insertMovie.js
@@ -1,9 +1,8 @@
-function InsertSampleMovie()
-{
-document.write('<object classid="clsid: 02BF25D5..." ...>\n');
-document.write('<param name="src" value="gravity-waves.mov" name="width" value="320" name="height" value="240" />\n');
-
-document.write('<embed src="gravity-waves.mov" src="gravity-waves.mov" width="320" height="240" autoplay="true" loop="true" controller="false" pluginspage="http://www.apple.com/quicktime/"></embed>\n');
-document.write('</object>\n');
-
+function InsertSampleMovie() {
+	document.write('<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" width="320" height="240" codebase="http://www.apple.com/qtactivex/qtplugin.cab">\n');
+	document.write('<param name="src" value="gravity-waves.mov" />\n');
+	document.write('<param name="autoplay" value="true" />\n');
+	document.write('<param name="controller" value="false" />\n');
+	document.write('<embed src="gravity-waves.mov" src="gravity-waves.mov" width="320" height="240" autoplay="true" loop="true" controller="false" pluginspage="http://www.apple.com/quicktime/"></embed>\n');
+	document.write('</object>\n');
 }
