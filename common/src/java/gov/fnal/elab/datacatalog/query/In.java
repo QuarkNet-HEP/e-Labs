7cbe0c0ad286cd974b71b4a2468e790ca07e5199
phongn <phongn@gmail.com>
2009-09-16 20:25:31 +0000

    PreparedStatement annotation lookup code update

---
+++ common/src/java/gov/fnal/elab/datacatalog/query/In.java
@@ -0,0 +1,10 @@
+/*
+ * Created on Mar 22, 2007
+ */
+package gov.fnal.elab.datacatalog.query;
+
+public class In extends MultiQueryElement {
+    public In() {
+        super(QueryElement.IN);
+    }
+}
