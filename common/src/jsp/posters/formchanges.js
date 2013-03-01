c62ed1880a608569d0500693bb121f864392b6bb
eperonja <edit.peronja@gmail.com>
2013-03-01 21:01:40 +0000

    499: updating javascript

--- common/src/jsp/posters/formchanges.js
+++ common/src/jsp/posters/formchanges.js
@@ -6,27 +6,21 @@
  * An empty array indicates no changes have been made.
  * NULL indicates that the form does not exist.
  * 
- * SetNotDirty()
- * Resets dirty flag for tinyMCE textareas
  * 
  */
 function FormChanges(form) {
 	// get form
-	alert("line 1");
 	if (typeof form == "string") form = document.getElementById(form);
 	if (!form || !form.nodeName || form.nodeName.toLowerCase() != "form") return null;
-	alert("line 2");
 	
 	// find changed elements
 	var changed = [], item, changedFlag, def, o, ol, opt;
 	
 	for (var e = 0, el = form.elements.length; e < el; e++) {
-		alert("for loop 1");
 		item = form.elements[e];
 		changedFlag = false;
 		switch (item.nodeName.toLowerCase()) {
 			case "select":
-				alert("switch select");
 				def = 0;
 				for (o = 0, ol = item.options.length; o < ol; o++) {
 					opt = item.options[o];
@@ -36,15 +30,7 @@ function FormChanges(form) {
 				if (changedFlag && !item.multiple) changedFlag = (def != item.selectedIndex);
 				break;
 			case "textarea":
-				alert("switch textarea");
-				if (tinyMCE.get(item.id).getContent().trim() == '') {
-					changedFlag = false;
-				} else {
-					changedFlag = tinyMCE.getInstanceById(item.id).isDirty;
-				}
-				break;
 			case "input":
-				alert("switch input");
 				changedFlag = (item.value != item.defaultValue);
 			    break;
 			default:
@@ -54,21 +40,4 @@ function FormChanges(form) {
 		if (changedFlag) changed.push(item);
 	}
 	return changed;
-}
-
-//EPeronja-02/15/2013: Bug499- tinyMCE textareas are different from regular textareas
-function SetNotDirty() {
-	// get form
-	if (typeof form == "string") form = document.getElementById(form);
-	if (!form || !form.nodeName || form.nodeName.toLowerCase() != "form") return null;
-	
-	for (var e = 0, el = form.elements.length; e < el; e++) {
-		var n = form.elements[e];
-		switch (n.nodeName.toLowerCase()) {
-			case "textarea":
-				var notDirty = tinyMCE.get(n.id).getContent();
-				notDirty.isNotDirty = true;
-				break;
-		}
-	}
 }
