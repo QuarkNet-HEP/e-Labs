0000207d2430a2d205c696397a3c85eb244d3500
eperonja <edit.peronja@gmail.com>
2013-04-06 19:59:37 +0000

    added initial file-size based estimation code

--- cosmic/src/java/gov/fnal/elab/cosmic/estimation/CosmicAnalysisRunTimeEstimatorSet.java
+++ cosmic/src/java/gov/fnal/elab/cosmic/estimation/CosmicAnalysisRunTimeEstimatorSet.java
@@ -6,53 +6,93 @@ package gov.fnal.elab.cosmic.estimation;
 import gov.fnal.elab.estimation.ConstantEstimator;
 import gov.fnal.elab.estimation.EstimatorSet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CosmicAnalysisRunTimeEstimatorSet extends EstimatorSet {
-    public CosmicAnalysisRunTimeEstimatorSet() {
-        addEstimator("swift", "local", "I2U2.Cosmic::PerformanceStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::PerformanceStudy", "local", 0.1747, 3483));
-        addEstimator("swift", "i2u2", "I2U2.Cosmic::PerformanceStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::PerformanceStudy", "i2u2", 0.3323, 7000));
-        addEstimator("swift", "grid", "I2U2.Cosmic::PerformanceStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::PerformanceStudy", "grid", 0.1480, 70351));
-        addEstimator("swift", "mixed", "I2U2.Cosmic::PerformanceStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::PerformanceStudy", "mixed", 0.2008, 3913));
-
-        addEstimator("swift", "local", "I2U2.Cosmic::LifetimeStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::LifetimeStudy", "local", 0.2502, 3356));
-        addEstimator("swift", "i2u2", "I2U2.Cosmic::LifetimeStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::LifetimeStudy", "i2u2", 0.4755, 8436));
-        addEstimator("swift", "grid", "I2U2.Cosmic::LifetimeStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::LifetimeStudy", "grid", 0.2870, 120000));
-        // TODO this is a wild guess
-        addEstimator("swift", "mixed", "I2U2.Cosmic::LifetimeStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::LifetimeStudy", "mixed", 0.35, 4000));
-
-        addEstimator("swift", "local", "I2U2.Cosmic::FluxStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::FluxStudy", "local", 0.4320, 3469));
-        addEstimator("swift", "i2u2", "I2U2.Cosmic::FluxStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::FluxStudy", "i2u2", 1.0400, 8641));
-        addEstimator("swift", "grid", "I2U2.Cosmic::FluxStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::FluxStudy", "grid", 0.4000, 96000));
-        addEstimator("swift", "mixed", "I2U2.Cosmic::FluxStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::FluxStudy", "mixed", 0.6720, 9833));
+    
+    private static final Map<String, Integer> CORES_MAP;
+    private static final Map<String, Double> EFFICIENCY_MAP;
+    
+    static {
+        CORES_MAP = new HashMap<String, Integer>();
+        EFFICIENCY_MAP = new HashMap<String, Double>();
         
-        addEstimator("swift", "local", "I2U2.Cosmic::ShowerStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::ShowerStudy", "local", 0.2253, 2427));
-        addEstimator("swift", "i2u2", "I2U2.Cosmic::ShowerStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::ShowerStudy", "i2u2", 0.4480, 9050));
-        addEstimator("swift", "grid", "I2U2.Cosmic::ShowerStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::ShowerStudy", "grid", 0.24, 150000));
-        addEstimator("swift", "mixed", "I2U2.Cosmic::ShowerStudy",
-                new SelfAdjustingLinearEventEstimator("I2U2.Cosmic::ShowerStudy", "mixed", 0.35, 8000));
+        CORES_MAP.put("local", Runtime.getRuntime().availableProcessors());
+        //TODO Replace with actual core count in the cluster
+        CORES_MAP.put("i2u2", 16);
+        //TODO The model is probably broken in this case
+        CORES_MAP.put("grid", 256);
+        CORES_MAP.put("mixed", 300);
+       
+        /*
+         * Empirically determined. Essentially there the slope of the runtime vs. file size fit 
+         * for one raw data (m1) and the slope for the same when the analysis is run with two data
+         * files (m2), and the efficiency is m2/m1
+         */
+        //TODO Guessed
+        EFFICIENCY_MAP.put("local", 0.75);
+        // Measured
+        EFFICIENCY_MAP.put("i2u2", 0.75);
+        //TODO Guessed
+        EFFICIENCY_MAP.put("grid", 0.3);
+        //TODO Guessed
+        EFFICIENCY_MAP.put("mixed", 0.5);
+    }
+    
+    public CosmicAnalysisRunTimeEstimatorSet() {
+        // TODO Cluster values are measured on www13, the others are just the assumption
+        // that the other machines have comparable hardware
+        addSwiftFileSizeEstimator("local", "I2U2.Cosmic::PerformanceStudy", 0.002749625, /*TODO*/3483);
+        addSwiftFileSizeEstimator("i2u2", "I2U2.Cosmic::PerformanceStudy", 0.002749625, 6000);
+        addSwiftFileSizeEstimator("grid", "I2U2.Cosmic::PerformanceStudy", 0.002749625, /*TODO*/70351);
+        addSwiftFileSizeEstimator("mixed", "I2U2.Cosmic::PerformanceStudy", 0.002749625, /*TODO*/3913);
+
+        addSwiftFileSizeEstimator("local", "I2U2.Cosmic::LifetimeStudy", 0.003823715, /*TODO*/118000);
+        addSwiftFileSizeEstimator("i2u2", "I2U2.Cosmic::LifetimeStudy", 0.003823715, 118000);
+        addSwiftFileSizeEstimator("grid", "I2U2.Cosmic::LifetimeStudy", 0.003823715, /*TODO*/120000);
+        addSwiftFileSizeEstimator("mixed", "I2U2.Cosmic::LifetimeStudy", 0.003823715, /*TODO*/4000);
+
+        addSwiftFileSizeEstimator("local", "I2U2.Cosmic::FluxStudy", 0.001674764, /*TODO*/1000);
+        addSwiftFileSizeEstimator("i2u2", "I2U2.Cosmic::FluxStudy", 0.001674764, 1000);
+        addSwiftFileSizeEstimator("grid", "I2U2.Cosmic::FluxStudy", 0.001674764, /*TODO*/96000);
+        addSwiftFileSizeEstimator("mixed", "I2U2.Cosmic::FluxStudy", 0.001674764, /*TODO*/9833);
+        
+        addSwiftFileSizeEstimator("local", "I2U2.Cosmic::ShowerStudy", 0.003932321, 1000);
+        addSwiftFileSizeEstimator("i2u2", "I2U2.Cosmic::ShowerStudy", 0.003932321, 6000);
+        addSwiftFileSizeEstimator("grid", "I2U2.Cosmic::ShowerStudy", 0.003932321, /*TODO*/150000);
+        addSwiftFileSizeEstimator("mixed", "I2U2.Cosmic::ShowerStudy", 0.003932321, /*TODO*/8000);
 
 
         addEstimator("swift", "local", "I2U2.Cosmic::EventPlot",
-                new ConstantEstimator(4));
+                new ConstantEstimator(1));
         addEstimator("swift", "i2u2", "I2U2.Cosmic::EventPlot",
                 new ConstantEstimator(4));
         addEstimator("swift", "grid", "I2U2.Cosmic::EventPlot",
                 new ConstantEstimator(4));
         addEstimator("swift", "mixed", "I2U2.Cosmic::EventPlot",
                 new ConstantEstimator(4));
+    }
+
+    private void addSwiftFileSizeEstimator(String method, String type, double aguess, double bguess) {
+        addEstimator("swift", method, type, 
+                new SelfAdjustingLinearFileSizeEstimator(type, method, 
+                        aguess, bguess, getCores(method), getEfficiency(method)));
+    }
+
+    private int getCores(String method) {
+        Integer v = CORES_MAP.get(method);
+        if (v == null) {
+            throw new IllegalArgumentException("No core count found for: " + method);
+        }
+        return v;
+    }
+
+    private double getEfficiency(String method) {
+        Double v = EFFICIENCY_MAP.get(method);
+        if (v == null) {
+            throw new IllegalArgumentException("No efficiency found for: " + method);
+        }
+        return v;
     }
 }
