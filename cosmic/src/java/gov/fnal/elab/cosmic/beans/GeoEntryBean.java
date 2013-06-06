e26e7eeb1c438eaa57ddc24268d7698691b98bcb
eperonja <edit.peronja@gmail.com>
2013-06-06 20:54:22 +0000

    Bugs fixes: 349 and 378

--- cosmic/src/java/gov/fnal/elab/cosmic/beans/GeoEntryBean.java
+++ cosmic/src/java/gov/fnal/elab/cosmic/beans/GeoEntryBean.java
@@ -96,9 +96,37 @@ public class GeoEntryBean implements Serializable {
     }
 
     public Date getDate() {
-        return getCalendar().getTime();
+        //return getCalendar().getTime();
+    	//EPeronja-06/06/2013:Bug 349 GEO time defaults selects wrong zone
+    	Date dateUTC = StringDateToDate(GetUTCdatetimeAsString());
+    	return dateUTC;
+    }
+    
+	//EPeronja-06/06/2013:Bug 349 GEO time defaults selects wrong zone
+    public static String GetUTCdatetimeAsString()
+    {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcTime = sdf.format(new Date());
+        return utcTime;
     }
 
+	//EPeronja-06/06/2013:Bug 349 GEO time defaults selects wrong zone
+    public static Date StringDateToDate(String StrDate)
+    {
+        Date dateToReturn = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        try
+        {
+            dateToReturn = (Date)dateFormat.parse(StrDate);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return dateToReturn;
+    }
+    
     public void setDetectorID(int detectorID) {
         this.detectorID = detectorID;
     }
@@ -565,7 +593,15 @@ public class GeoEntryBean implements Serializable {
             addError("channel" + channel + "-area");
             return false;
         }
-        else {
+        else { 
+        	boolean needCheck = getChannel(channel).needToCheckArea();
+        	if (needCheck) {
+        		//EPeronja-06/06/2013:Bug 378 GEO: default area value
+        		if(getChannel(channel).getArea().equals("0.0")) {
+        			addError("channel"+channel+"-area-zero");
+        			return false;
+        		}
+        	}
             return true;
         }
     }
@@ -737,7 +773,7 @@ public class GeoEntryBean implements Serializable {
         for (int i = 0; i < 4; i++) {
             channels[i] = new ChannelProperties();
         }
-        gpsCableLength = "0";
+        gpsCableLength = "0.0";
     }
 
     public String writeForFile() {
@@ -841,7 +877,7 @@ public class GeoEntryBean implements Serializable {
             y = "0";
             z = "0";
             area = "0.0";
-            cableLength = "0";
+            cableLength = "0.0";
         }
 
         public ChannelProperties(String length, String area, String x,
@@ -898,6 +934,12 @@ public class GeoEntryBean implements Serializable {
                     || !"0.0".equals(area) || !"0.0".equals(cableLength);
         }
 
+		//EPeronja-06/06/2013:Bug 378 GEO: default area value
+        public boolean needToCheckArea() {
+        	//only check when the others are not zeros
+            return !"0".equals(x) || !"0".equals(y) || !"0".equals(z) || !"0.0".equals(cableLength);
+        }
+       
         public String toString() {
             double a = Double.parseDouble(area);
             a = a / 100 / 100;
