6467c1b0eb65200249082284d593fd21a430f5c1
eperonja <edit.peronja@gmail.com>
2013-06-11 18:23:06 +0000

    254: Check plot dependencies before deleting

--- common/src/java/gov/fnal/elab/datacatalog/DataTools.java
+++ common/src/java/gov/fnal/elab/datacatalog/DataTools.java
@@ -13,13 +13,20 @@ import gov.fnal.elab.Elab;
 import gov.fnal.elab.datacatalog.StructuredResultSet.File;
 import gov.fnal.elab.datacatalog.StructuredResultSet.Month;
 import gov.fnal.elab.datacatalog.StructuredResultSet.School;
+import gov.fnal.elab.datacatalog.query.Between;
 import gov.fnal.elab.datacatalog.query.CatalogEntry;
+import gov.fnal.elab.datacatalog.query.Equals;
+import gov.fnal.elab.datacatalog.query.In;
+import gov.fnal.elab.datacatalog.query.Like;
+import gov.fnal.elab.datacatalog.query.And;
 import gov.fnal.elab.datacatalog.query.ResultSet;
+import gov.fnal.elab.util.DatabaseConnectionManager;
 import gov.fnal.elab.util.ElabException;
 import gov.fnal.elab.util.ElabUtil;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.time.DateFormatUtils;
+import org.apache.commons.codec.net.URLCodec;
 
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -34,6 +41,10 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * A few convenience functions for dealing with QuarkNet data
@@ -254,7 +265,50 @@ public class DataTools {
         srs.setEndDate(endDate);
         return srs;
     }
-
+    //EPeronja-06/11/2013: 254-When deleting files, be sure there are not dependent files
+    //					   This function will check plots in the logbook and posters
+    public static int checkPlotDependency(Elab elab, String plotName, int figureNumber) throws ElabException {
+    	int count = 0;
+		Connection con = null;
+		PreparedStatement ps = null;
+		//check logbook first
+		try {
+			con = DatabaseConnectionManager.getConnection(elab.getProperties()); 
+			
+			ps = con.prepareStatement(
+					"SELECT count(*) as COUNT " +
+				    "  FROM log " +
+					"WHERE log_text like ?;");
+            URLCodec urlCodec = new URLCodec();
+            String fileName = plotName;
+            try {
+            	fileName = urlCodec.encode(plotName);
+            } catch (Exception e) {
+            	throw new ElabException("Problem with encoding the name in DataTools.checkPlotDependency().");
+            }
+            ps.setString(1, "%"+fileName+"%");			
+			java.sql.ResultSet rs = ps.executeQuery(); 
+			if (rs.next()) {
+				count = rs.getInt(1);
+			}
+		}
+		catch (SQLException e) {
+        	throw new ElabException("In DataTools.checkPlotDependency(): " + e.getMessage());
+		}
+		finally {
+			DatabaseConnectionManager.close(con, ps);
+		}		
+		
+		In and = new In();
+		and.add(new Like("type","poster"));
+		and.add(new Like("FIG:FIGURE" + String.valueOf(figureNumber), plotName));
+		ResultSet rs = elab.getDataCatalogProvider().runQuery(and);
+		if (rs.size() > 0) {
+			count = count + rs.size();
+		}		
+    	return count;
+    }//end of checkPlotDependency()    
+    
     public static final DateFormat TZ_DATE_TIME_FORMAT;
 
     static {
