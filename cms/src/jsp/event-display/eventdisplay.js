d071476e2a02188b6f3619dab8ae81f9dd779606
tpmccauley <tpmccauley@gmail.com>
2013-07-31 16:32:29 +0000

    commit changes from 2.0

--- cms/src/jsp/event-display/eventdisplay.js
+++ cms/src/jsp/event-display/eventdisplay.js
@@ -192,11 +192,22 @@ function initializeData() {
     }
     switch (type) {
     case TRACK:
+  		lines = fn(edata, rd, desc, dataref, d_event["Associations"][desc.assoc]);
+      	for (var k = 0; k < lines.length; k++) {
+	 		vec.push(lines[k]);
+      	}
+      	break;
     case CURVES:
     case PATHS:
+    	if (edata.length != d_event["Associations"][desc.assoc].length) {
+    		console.log(edata.length, d_event["Associations"][desc.assoc].length, desc);
+    		break;
+    	}
+    	
       lines = fn(edata, rd, desc, dataref, d_event["Associations"][desc.assoc]);
+
       for (var k = 0; k < lines.length; k++) {
-	vec.push(lines[k]);
+	 	vec.push(lines[k]);
       }
       break;
     case LINES:
