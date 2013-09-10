86a0ca614ac4ebb3d91e3ce19c05c60d39066c5d
eperonja <edit.peronja@gmail.com>
2013-09-10 18:08:35 +0000

    testing notifications

--- common/src/java/gov/fnal/elab/notifications/ElabNotificationsProvider.java
+++ common/src/java/gov/fnal/elab/notifications/ElabNotificationsProvider.java
@@ -19,6 +19,8 @@ public interface ElabNotificationsProvider extends ElabProviderHandled {
     List<Notification> getNotifications(ElabGroup group, int max, boolean includeOld) throws ElabException;
     
     List<Notification> getSystemNotifications(int count) throws ElabException;
+
+    List<Notification> getSystemNotifications() throws ElabException;    
     
     void addNotification(ElabGroup eg, Notification n) throws ElabException; 
     
