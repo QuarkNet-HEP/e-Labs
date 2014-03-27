b3216c77677a63e8511f19bd5e295b906df38377
eperonja <edit.peronja@gmail.com>
2014-03-27 01:48:15 +0000

    Bless Process: fixing bugs with fail reason

--- cosmic/src/java/gov/fnal/elab/cosmic/bless/BlessProcess.java
+++ cosmic/src/java/gov/fnal/elab/cosmic/bless/BlessProcess.java
@@ -113,36 +113,44 @@ public class BlessProcess {
 										failReason = formatFailReason(split[0], "channel 1", String.valueOf(chan1Rate), split[1], split[2]);
 									}
 									//compare channel 2 and see if file can be blessed
-									if (chan2Rate <= (parseToDouble(split[3]) + parseToDouble(split[4])) && chan2Rate >= (parseToDouble(split[3]) - parseToDouble(split[4])) && pass) {
-										pass = true;
-									} else {
-										pass = false;
-										failReason = formatFailReason(split[0], "channel 2", String.valueOf(chan2Rate), split[3], split[4]);
+									if (pass) {
+										if (chan2Rate <= (parseToDouble(split[3]) + parseToDouble(split[4])) && chan2Rate >= (parseToDouble(split[3]) - parseToDouble(split[4]))) {
+											pass = true;
+										} else {
+											pass = false;
+											failReason = formatFailReason(split[0], "channel 2", String.valueOf(chan2Rate), split[3], split[4]);
+										}
 									}
 									//compare channel 3 and see if file can be blessed
-									if (chan3Rate <= (parseToDouble(split[5]) + parseToDouble(split[6])) && chan3Rate >= (parseToDouble(split[5]) - parseToDouble(split[6])) && pass) {
-										pass = true;
-									} else {
-										pass = false;
-										failReason = formatFailReason(split[0], "channel 3", String.valueOf(chan3Rate), split[5], split[6]);
+									if (pass) {
+										if (chan3Rate <= (parseToDouble(split[5]) + parseToDouble(split[6])) && chan3Rate >= (parseToDouble(split[5]) - parseToDouble(split[6]))) {
+											pass = true;
+										} else {
+											pass = false;
+											failReason = formatFailReason(split[0], "channel 3", String.valueOf(chan3Rate), split[5], split[6]);
+										}
 									}
 									//compare channel 4 and see if file can be blessed
-									if (chan4Rate <= (parseToDouble(split[7]) + parseToDouble(split[8])) && chan4Rate >= (parseToDouble(split[7]) - parseToDouble(split[8])) && pass) {
-										pass = true;
-									} else {
-										pass = false;
-										failReason = formatFailReason(split[0], "channel 4", String.valueOf(chan4Rate), split[7], split[8]);
+									if (pass) {
+										if (chan4Rate <= (parseToDouble(split[7]) + parseToDouble(split[8])) && chan4Rate >= (parseToDouble(split[7]) - parseToDouble(split[8]))) {
+											pass = true;
+										} else {
+											pass = false;
+											failReason = formatFailReason(split[0], "channel 4", String.valueOf(chan4Rate), split[7], split[8]);
+										}
 									}
 									//compare triggers and see if file can be blessed
 									//if the trigger + triggerError < 2, we are not going to bother comparing
 									//this was decided on the Nov 13 2013 telecon
 									//low trigger rates alone shouldn't fail a file
-									if ((parseToDouble(split[9]) + parseToDouble(split[10])) >= 2 && pass) {
-										if (triggerRate < (parseToDouble(split[9]) + parseToDouble(split[10])) && triggerRate > (parseToDouble(split[9]) - parseToDouble(split[10])) ) {
-											pass = true;
-										} else {
-											pass = false;
-											failReason = formatFailReason(split[0], "trigger", String.valueOf(triggerRate), split[9], split[10]);
+									if (pass) {
+										if ((parseToDouble(split[9]) + parseToDouble(split[10])) >= 2) {
+											if (triggerRate < (parseToDouble(split[9]) + parseToDouble(split[10])) && triggerRate > (parseToDouble(split[9]) - parseToDouble(split[10])) ) {
+												pass = true;
+											} else {
+												pass = false;
+												failReason = formatFailReason(split[0], "trigger", String.valueOf(triggerRate), split[9], split[10]);
+											}
 										}
 									}
 								}
