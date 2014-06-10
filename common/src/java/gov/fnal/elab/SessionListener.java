2bc12da7102956302d1580000bd4d69021affed6
eperonja <edit.peronja@gmail.com>
2014-06-10 21:10:44 +0000

    Session Listener: cleaned code

--- common/src/java/gov/fnal/elab/SessionListener.java
+++ common/src/java/gov/fnal/elab/SessionListener.java
@@ -1,6 +1,5 @@
 package gov.fnal.elab;
 
-//import gov.fnal.elab.Pair;
 import java.util.*;
 import java.text.*;
 import javax.servlet.http.HttpSession;
@@ -10,15 +9,11 @@ import javax.servlet.http.HttpSessionListener;
 public class SessionListener implements HttpSessionListener {
     private static int sessionCount;
     private static ArrayList sessions = new ArrayList();
-    //private static TreeMap<String, List<Pair>> sessions = new TreeMap<String, List<Pair>>();
     DateFormat df = new SimpleDateFormat("MMM dd yyyy HH:MM:SS");
 	
     public static int getTotalActiveSession() {
     	return sessionCount;
     }
-    //public static TreeMap<String, List<Pair>> getTotalSessionUsers() {
-    //	return sessions;
-    //}
     public static ArrayList getTotalSessionUsers() {
     	return sessions;
     }
@@ -26,12 +21,8 @@ public class SessionListener implements HttpSessionListener {
     public void sessionCreated(HttpSessionEvent event) {
         synchronized (this) {
         	sessionCount++;
-            //List<Pair> sessionDetails = new ArrayList<Pair>();
         	HttpSession s = event.getSession();
         	sessions.add(s);
-        	//sessionDetails.add(new Pair("Session", s));
-        	//sessionDetails.add(new Pair("Start-Time", df.format(new Date())));
-        	//sessions.put(s.getId(), sessionDetails);
         }
     }
     @Override
@@ -40,16 +31,7 @@ public class SessionListener implements HttpSessionListener {
         	sessionCount--;
         	HttpSession s = event.getSession();
         	sessions.remove(s);
-        	//sessions.remove(s.getId());
         }
     }		
     
-    public static void invalidateSession(String sessionId) {
-    	//List<Pair> x = sessions.remove(sessionId);
-    	//Pair y = x.get(0);
-    	//if (y.getLeft().equals("session")) {
-    	//	HttpSession s = (HttpSession) y.getRight();
-    	//	s.invalidate();
-    	//}
-    }	
 }
