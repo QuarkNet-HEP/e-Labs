a698a989849ca0367b54a8feb03977f5444b2616
eperonja <edit.peronja@gmail.com>
2014-11-14 20:56:05 +0000

    By Mihael: update import tools to allow both 60s and 600s second-trend file types

--- ligo/src/java/gov/fnal/elab/ligo/data/convert/LIGOFile.java
+++ ligo/src/java/gov/fnal/elab/ligo/data/convert/LIGOFile.java
@@ -12,12 +12,26 @@ public class LIGOFile implements Comparable<LIGOFile> {
     public static final int MINUTE_TREND = 0;
     
     public final int trend, site;
+    private int duration;
+    private long startTime;
     public final File file;
    
     public LIGOFile(int site, int trend, File f) {
         this.site = site;
         this.trend = trend;
         this.file = f;
+        parse();
+    }
+
+    public static final Pattern RE_FILE_GPS_TIME = Pattern.compile("[H|L]-[T|M]-(\\d+)-(\\d+).gwf");
+    
+    private void parse() {
+        Matcher m = RE_FILE_GPS_TIME.matcher(this.file.getName());
+        if (!m.matches()) {
+            throw new IllegalArgumentException("Invalid ligo file (does not match expected pattern): " + file);
+        }
+        startTime = Long.parseLong(m.group(1));
+        duration = Integer.parseInt(m.group(2));
     }
 
     @Override
@@ -39,63 +53,62 @@ public class LIGOFile implements Comparable<LIGOFile> {
     public int hashCode() {
         return file.getName().hashCode() + trend * 113 + site * 9771;
     }
-    
-    public static final Pattern RE_FILE_GPS_TIME = Pattern.compile("[H|L]-[T|M]-(\\d+)-(\\d+).gwf");
 
     public int compareTo(LIGOFile o) {
         if (this.equals(o)) {
             return 0;
         }
-        Matcher m1 = RE_FILE_GPS_TIME.matcher(this.file.getName());
-        if (m1.matches()) {
-            long t1 = Long.parseLong(m1.group(1));
-            int d1 = Integer.parseInt(m1.group(2));
-            Matcher m2 = RE_FILE_GPS_TIME.matcher(o.file.getName());
-            if (m2.matches()) {
-                long t2 = Long.parseLong(m2.group(1));
-                int d2 = Integer.parseInt(m2.group(2));
-                // <--> 1 (this)
-                // (--) 2
-                // -1 - this < o
-                //  0 - this == o
-                //  1 - this > o
-                // <---->--(------)
-                if (t1 + d1 <= t2) {
-                    return -1;
-                }
-                // (----)--<------>
-                if (t2 + d2 <= t1) {
-                    return 1;
-                }
-                // <-(----)--> or (--<---->---)
-                if (t1 <= t2 && t1 + d1 >= t2 + d2 || t2 <= t1 && t2 + d2 >= t1 + d1) {
-                    if (trend == SECOND_TREND) {
-                        return -1;
-                    }
-                    if (o.trend == SECOND_TREND) {
-                        return 1;
-                    }
-                    throw new RuntimeException("This should be unreachable 1 (" + this + ", " + o + ")");
-                }
-                // <--(---->--) or (--<----)-->
-                // this shouldn't actually be happening, since data is aligned
-                if (t1 <= t2 && t1 + d1 > t2 || t2 <= t1 && t2 + d2 > t1) {
-                    if (trend == SECOND_TREND) {
-                        return -1;
-                    }
-                    if (o.trend == SECOND_TREND) {
-                        return 1;
-                    }
-                    throw new RuntimeException("This should be unreachable 2 (" + this + ", " + o + ")");
-                }
-                throw new RuntimeException("This should be unreachable 3 (" + this + ", " + o + ")");
-            }
+        long t1 = this.startTime;
+        int d1 = this.duration;
+        long t2 = o.startTime;
+        int d2 = o.duration;
+        // <--> 1 (this)
+        // (--) 2
+        // -1 - this < o
+        //  0 - this == o
+        //  1 - this > o
+        // <---->--(------)
+        if (t1 + d1 <= t2) {
+            return -1;
         }
-        throw new RuntimeException("Invalid ligo file: " + file);
+        // (----)--<------>
+        if (t2 + d2 <= t1) {
+            return 1;
+        }
+        // <-(----)--> or (--<---->---)
+        if (t1 <= t2 && t1 + d1 >= t2 + d2 || t2 <= t1 && t2 + d2 >= t1 + d1) {
+            if (trend == SECOND_TREND) {
+                return -1;
+            }
+            if (o.trend == SECOND_TREND) {
+                return 1;
+            }
+            throw new RuntimeException("This should be unreachable 1 (" + this + ", " + o + ")");
+        }
+        // <--(---->--) or (--<----)-->
+        // this shouldn't actually be happening, since data is aligned
+        if (t1 <= t2 && t1 + d1 > t2 || t2 <= t1 && t2 + d2 > t1) {
+            if (trend == SECOND_TREND) {
+                return -1;
+            }
+            if (o.trend == SECOND_TREND) {
+                return 1;
+            }
+            throw new RuntimeException("This should be unreachable 2 (" + this + ", " + o + ")");
+        }
+        throw new RuntimeException("This should be unreachable 3 (" + this + ", " + o + ")");
     }
 
     @Override
     public String toString() {
         return file.getName();
+    }
+    
+    public int getFileDurationInSeconds() {
+        return duration;
+    }
+    
+    public long getFileGPSTime() {
+        return startTime;
     }
 }
