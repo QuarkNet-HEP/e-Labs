fc558d616f23fea669f3511568a78e5e28c6d3ec
eperonja <edit.peronja@gmail.com>
2015-01-23 18:51:27 +0000

    Added Tom M new code for ispy-webgl

---
+++ cms/src/jsp/ispy-webgl/js/load.js
@@ -0,0 +1,281 @@
+ispy.web_files = [
+  "./masterclass-2015/masterclass_1.ig",
+  "./masterclass-2015/masterclass_2.ig",
+  "./masterclass-2015/masterclass_3.ig",
+  "./masterclass-2015/masterclass_4.ig",
+  "./masterclass-2015/masterclass_5.ig",
+  "./masterclass-2015/masterclass_6.ig",
+  "./masterclass-2015/masterclass_7.ig",
+  "./masterclass-2015/masterclass_8.ig",
+  "./masterclass-2015/masterclass_9.ig",
+  "./masterclass-2015/masterclass_10.ig",
+  "./masterclass-2015/masterclass_11.ig",
+  "./masterclass-2015/masterclass_12.ig",
+  "./masterclass-2015/masterclass_13.ig",
+  "./masterclass-2015/masterclass_14.ig",
+  "./masterclass-2015/masterclass_15.ig",
+  "./masterclass-2015/masterclass_16.ig",
+  "./masterclass-2015/masterclass_17.ig",
+  "./masterclass-2015/masterclass_18.ig",
+  "./masterclass-2015/masterclass_19.ig",
+  "./masterclass-2015/masterclass_20.ig",
+  "./masterclass-2015/masterclass_21.ig",
+  "./masterclass-2015/masterclass_22.ig",
+  "./masterclass-2015/masterclass_23.ig",
+  "./masterclass-2015/masterclass_24.ig",
+  "./masterclass-2015/masterclass_25.ig",
+  "./masterclass-2015/masterclass_26.ig",
+  "./masterclass-2015/masterclass_27.ig",
+  "./masterclass-2015/masterclass_28.ig",
+  "./masterclass-2015/masterclass_29.ig",
+  "./masterclass-2015/masterclass_30.ig"
+];
+
+
+ispy.ig_data = null;
+
+ispy.openDialog = function(id) {
+  $(id).modal('show');
+}
+
+ispy.closeDialog = function(id) {
+  $(id).modal('hide');
+}
+
+ispy.hasFileAPI = function() {
+  if ( window.FileReader ) {
+    return true;
+  } else {
+    console.log("FileReader", window.FileReader);
+    console.log("File", window.File);
+    console.log("FileList", window.FileList);
+    console.log("FileSystem", window.FileSystem);
+    return false;
+  }
+}
+
+ispy.clearTable = function(id) {
+  var tbl = document.getElementById(id);
+  while (tbl.rows.length > 0) {
+    tbl.deleteRow(0);
+  }
+}
+
+ispy.selectEvent = function(index) {
+  $("#selected-event").html(ispy.event_list[index]);
+  ispy.event_index = index;
+}
+
+ispy.updateEventList = function() {
+  ispy.clearTable("browser-events");
+  var tbl = document.getElementById("browser-events");
+
+  for (var i = 0; i < ispy.event_list.length; i++) {
+    var e = ispy.event_list[i];
+    var row = tbl.insertRow(tbl.rows.length);
+    var cell = row.insertCell(0);
+    cell.innerHTML = '<a id="browser-event-' + i + '" class="event" onclick="ispy.selectEvent(\'' + i + '\');">' + e + '</a>';
+  }
+}
+
+ispy.enableNextPrev = function() {
+  if ( ispy.event_index > 0 ) {
+    $("#prev-event-button").removeClass("disabled");
+  }
+  else {
+    $("#prev-event-button").addClass("disabled");
+  }
+
+  if ( ispy.event_list && ispy.event_list.length - 1 > ispy.event_index ) {
+    $("#next-event-button").removeClass("disabled");
+  }
+  else {
+    $("#next-event-button").addClass("disabled");
+  }
+}
+
+ispy.loadEvent = function() {
+  $("#event-loaded").html("");
+  $("#progress").modal("show");
+
+  try {
+    var event = JSON.parse(ispy.cleanupData(ispy.ig_data.file(ispy.event_list[ispy.event_index]).asText()));
+  } catch(err) {
+    alert(err);
+  }
+
+  $("#progress").modal("hide");
+  ispy.addEvent(event);
+  ispy.enableNextPrev();
+
+  $("#event-loaded").html(ispy.file_name + ":" + ispy.event_list[ispy.event_index]);
+}
+
+ispy.nextEvent = function() {
+  if ( ispy.event_list && ispy.event_list.length-1 > ispy.event_index ) {
+    ispy.event_index++;
+    ispy.loadEvent();
+  }
+}
+
+ispy.prevEvent = function() {
+  if ( ispy.event_list && ispy.event_index > 0) {
+    ispy.event_index--;
+    ispy.loadEvent();
+  }
+}
+
+ispy.selectLocalFile = function(index) {
+  var reader = new FileReader();
+  ispy.file_name = ispy.local_files[index].name;
+
+  reader.onload = function(e) {
+    var data = e.target.result;
+    var zip = new JSZip(data);
+    var event_list = [];
+
+    $.each(zip.files, function(index, zipEntry){
+      if ( zipEntry._data !== null && zipEntry.name !== "Header" ) {
+        event_list.push(zipEntry.name);
+      }
+    });
+
+    ispy.event_list = event_list;
+    ispy.event_index = 0;
+    ispy.updateEventList();
+    ispy.ig_data = zip;
+  }
+
+  reader.onerror = function(e) {
+    alert(e);
+  }
+
+  reader.readAsArrayBuffer(ispy.local_files[index]);
+}
+
+ispy.updateLocalFileList = function(list) {
+  ispy.clearTable("browser-files");
+  var tbl = document.getElementById("browser-files");
+
+  for (var i = 0; i < list.length; i++) {
+    var name = list[i].name;
+    var row = tbl.insertRow(tbl.rows.length);
+    var cell = row.insertCell(0);
+    var cls = "file";
+    cell.innerHTML = '<a id="browser-file-' + i + '" class="' + cls + '" onclick="ispy.selectLocalFile(\'' + i + '\');">' + name + '</a>';
+  }
+}
+
+ispy.loadLocalFiles = function() {
+  if (!ispy.hasFileAPI()) {
+    var err_msg = "Sorry. You seeem to be using a browser that does not support FileReader API. ";
+    err_msg += "Please try with Chrome (6.0+), Firefox (3.6+), Safari (6.0+), or IE (10+). ";
+    err_msg += "Alternatively, open a file from the web. ";
+    alert(err_msg);
+    return;
+  }
+
+  ispy.clearTable("browser-files");
+  ispy.clearTable("browser-events");
+  $('#selected-event').html("Selected event");
+
+  ispy.local_files = document.getElementById('local-files').files;
+  ispy.updateLocalFileList(ispy.local_files);
+  ispy.openDialog('#files');
+}
+
+ispy.selectFile = function(filename) {
+  ispy.file_name = filename.split("/")[2];  // of course this isn't a general case for files
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", filename, true);
+  xhr.overrideMimeType("text/plain; charset=x-user-defined");
+
+  ispy.clearTable("browser-events");
+  var ecell = document.getElementById("browser-events").insertRow(0).insertCell(0);
+  ecell.innerHTML = 'Loading events...';
+
+  xhr.onload = function() {
+    if (this.status == 200) {
+
+      var zip = JSZip(xhr.responseText);
+      var event_list = [];
+
+      $.each(zip.files, function(index, zipEntry){
+        if ( zipEntry._data !== null && zipEntry.name !== "Header" ) {
+          event_list.push(zipEntry.name);
+        }
+      });
+
+      ispy.event_list = event_list;
+      ispy.event_index = 0;
+      ispy.updateEventList();
+      ispy.ig_data = zip;
+    }
+  };
+
+  xhr.send();
+}
+
+ispy.loadWebFiles = function() {
+  ispy.clearTable("browser-files");
+  ispy.clearTable("browser-events");
+  $('#selected-event').html("Selected event");
+
+  var tbl = document.getElementById("browser-files");
+
+  for (var i = 0; i < ispy.web_files.length; i++) {
+    var e = ispy.web_files[i];
+    var name = e.split("/")[2];
+    var row = tbl.insertRow(tbl.rows.length);
+    var cell = row.insertCell(0);
+    var cls = "file";
+    cell.innerHTML = '<a id="browser-file-' + i + '" class="' + cls + '" onclick="ispy.selectFile(\'' + e + '\');">' + name + '</a>';
+  }
+}
+
+ispy.cleanupData = function(d) {
+  // rm non-standard json bits
+  // newer files will not have this problem
+  d = d.replace(/\(/g,'[')
+       .replace(/\)/g,']')
+       .replace(/\'/g, "\"")
+       .replace(/nan/g, "0");
+  return d;
+}
+
+ispy.exportString = function (output) {
+  // This comes from three.js editor
+  var blob = new Blob([output], {type: 'text/plain'});
+  var objectURL = URL.createObjectURL(blob);
+  window.open(objectURL, '_blank');
+  window.focus();
+}
+
+ispy.exportScene = function() {
+  // Only export objects in the scene that are visible.
+  // Each visible object is exported to its own tab (A feature, not a bug! For now.)
+  // WARNING! This works best (and is intended for) output of solid geometries.
+  // For tracks this will create a separate file for each track and wireframe
+  // geometries will only output vertices, connected in sequence. Use with care!
+  ispy.scene.children.forEach(function(c){
+    c.children.forEach(function(o){
+      if ( o.visible ) {
+        var output = o.toJSON();
+        output = JSON.stringify( output, null, '\t' );
+        output = output.replace( /[\n\t]+([\d\.e\-\[\]]+)/g, '$1' );
+        ispy.exportString(output);
+      }
+    });
+  });
+}
+
+// Instead of a button, make output of 3D to JSON a "secret" key binding
+document.addEventListener('keydown', function(e) {
+  e.preventDefault();
+
+  // If shift + e then export
+  if ( e.which === 69 && e.shiftKey ) {
+    ispy.exportScene();
+  }
+});
