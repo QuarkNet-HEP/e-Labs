87290fc0de33bacc68ceee61eee7c1ffc7af145d
eperonja <edit.peronja@gmail.com>
2015-05-04 20:31:40 +0000

    643-Concatenate blessing plots for flux

---
+++ common/src/jsp/include/json/json.async.js
@@ -0,0 +1,19 @@
+JSON.parseAsync = function(data, callback)
+{var worker, json
+	if( window.Worker )
+	{
+		worker = new Worker( '../include/json/json.worker.js' );
+		worker.addEventListener( 'message', function (e)
+		{
+			json = e.data;
+			callback( json );
+		}, false);
+		worker.postMessage( data );
+		return;
+	}
+	else
+	{
+		json = JSON.parse( data );
+		callback( json );
+	}
+};
