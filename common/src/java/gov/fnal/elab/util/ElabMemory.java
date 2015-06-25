7ee100f88bf5df3700043964dbaff5150ec1bb57
eperonja <edit.peronja@gmail.com>
2015-06-25 18:26:13 +0000

    OOME: added notification

--- common/src/java/gov/fnal/elab/util/ElabMemory.java
+++ common/src/java/gov/fnal/elab/util/ElabMemory.java
@@ -1,4 +1,5 @@
 package gov.fnal.elab.util;
+import gov.fnal.elab.*;
 
 public class ElabMemory {
 	int megabytes = 1024*1024;
@@ -39,5 +40,24 @@ public class ElabMemory {
 		}
 		return isCritical;
 	}
-	
+	public void notifyAdmin(Elab elab, String message) {
+    	//send email with warning
+       	String to = elab.getProperty(elab.getName() + ".notifyAnalysisFailureTO");
+    	if (to == null) {
+    		to="help@i2u2.org";
+    	}
+    	String cc = elab.getProperty(elab.getName() + ".notifyAnalysisFailureCC");
+		String emailSubject = "EventCandidates: stopped analysis";
+		String emailBody =  message;
+	    try {
+	    	String result = elab.getUserManagementProvider().sendEmail(to, emailSubject, emailBody);
+        	if (cc != null) {
+        		result = elab.getUserManagementProvider().sendEmail(cc, emailSubject, emailBody);
+        	}
+	    } catch (Exception ex) {
+            System.err.println("Failed to send email");
+            ex.printStackTrace();
+	    }		    		
+		
+	}//end of notify
 }// end of ElabMemory
