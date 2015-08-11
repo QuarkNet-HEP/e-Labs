9ea060c7664d6c454cac13c785fec2c10a0289b4
eperonja <edit.peronja@gmail.com>
2015-08-11 03:43:14 +0000

    Time of Flight: updated swift

--- cosmic/src/swift/I2U2.Cosmic::TimeOfFlight.swift
+++ cosmic/src/swift/I2U2.Cosmic::TimeOfFlight.swift
@@ -45,38 +45,6 @@ type AxisParams {
 	}
 }
 
-
-(File image, File outfile_param) Plot(string ptype, string caption, AxisParams x, AxisParams y, 
-	AxisParams z, string title, File infile, File extraFun) {
-	
-	app {
-		Plot 
-			"-file" @filename(infile)
-			"-extra" @filename(extraFun)
-			"-param" @filename(outfile_param)
-			"-svg" @filename(image)
-			"-type" ptype
-			"-title" title
-			"-xlabel" x.label
-			"-ylabel" y.label
-			"-zlabel" z.label
-			"-caption" caption
-			"-lowx" x.low
-			"-highx" x.high
-			"-lowy" y.low
-			"-highy" y.high
-			"-lowz" z.low
-			"-highz" z.high;
-	}
-}
-
-(File png) SVG2PNG(File svg, string height) {
-	app {
-		SVG2PNG "-h" height "-w" height @filename(svg) @filename(png);
-	}
-}
-
-
 string channelCoincidence = @arg("channelCoincidence");
 string detectorCoincidence = @arg("detectorCoincidence");
 string eventCoincidence = @arg("eventCoincidence");
@@ -89,41 +57,22 @@ File eventFile <single_file_mapper;file=@arg("eventFile")>;
 
 File rawData[] <fixed_array_mapper;files=@arg("rawData")>;
 File thresholdAll[] <fixed_array_mapper;files=@arg("thresholdAll")>;
-//This is done to avoid corruption of threshold files when created
-//concurrently by multiple runs
-//File thresholdAll[] <structured_regexp_mapper;source=rawData,match=".*/(.*)",transform="\\1.thresh">;
 File wireDelayData[] <fixed_array_mapper;files=@arg("wireDelayData")>;
 
 string  detectors[] = @strsplit(@arg("detector"), "\\s");
 string  cpldfreqs[] = @strsplit(@arg("cpldfreqs"), "\\s");
 string  firmwares[] = @strsplit(@arg("firmwares"), "\\s");
+string 	singleChannel_require = @arg("singleChannel_require");
+string 	singleChannel_veto = @arg("singleChannel_veto");
 
 File combineOut <single_file_mapper;file=@arg("combineOut")>;
 File sortOut <single_file_mapper;file=@arg("sortOut")>;
 
 string geoDir = @arg("geoDir");
 File geoFiles[] <fixed_array_mapper;files=@arg("geoFiles")>;
-
-AxisParams x, y, z;
-
-x.high = @arg("plot_highX");
-x.low  = @arg("plot_lowX");
-x.label = @arg("plot_xlabel");
-
-y.high = @arg("plot_highY");
-y.low  = @arg("plot_lowY");
-y.label = @arg("plot_ylabel");
-
-z.high = @arg("plot_highZ");
-z.low  = @arg("plot_lowZ");
-z.label = @arg("plot_zlabel");
-
-
-string plot_plot_type = @arg("plot_plot_type");
 string plot_title = @arg("plot_title");
 string sort_sortKey1 = @arg("sort_sortKey1");
 string sort_sortKey2 = @arg("sort_sortKey2");
-
 
 //the actual workflow
 wireDelayData = WireDelayMultiple(thresholdAll, geoDir, geoFiles, detectors, firmwares);
