9b52f0138947f61a2a181f69f8c4451432f8e8f1
eperonja <edit.peronja@gmail.com>
2016-01-19 17:18:26 +0000

    Ispy-webgl: update by Tom

--- cms/src/jsp/ispy-webgl/js/setup.js
+++ cms/src/jsp/ispy-webgl/js/setup.js
@@ -1,10 +1,7 @@
-var ispy = ispy || {};
-ispy.detector = {"Collections":{}};
-ispy.version = "0.9-masterclass2015-rc6";
-
 ispy.hasWebGL = function() {
   var canvas = document.createElement('canvas');
-
+  
+  /*
   if ( ! canvas.getContext('webgl') ) {
     console.log('no webgl');
   } else {
@@ -22,6 +19,7 @@ ispy.hasWebGL = function() {
   } else {
     console.log('WebGLRenderingContext');
   }
+  */
 
   if ( canvas.getContext('webgl') || canvas.getContext('experimental-webgl') ) {
     if ( ! window.WebGLRenderingContext ) {
@@ -33,7 +31,30 @@ ispy.hasWebGL = function() {
   } else {
     return false;
   }
-}
+};
+
+ispy.lookAtOrigin = function() {
+  ispy.camera.lookAt(new THREE.Vector3(0,0,0));
+};
+
+ispy.setFramerate = function(fr) {
+  ispy.framerate = fr;
+  $('#fr').html(fr);
+};
+
+ispy.initCamera = function() {
+  var home_x = -18.1;
+  var home_y = 8.6;
+  var home_z = 14.0;
+
+  ispy.camera.position.x = home_x*0.6;
+  ispy.camera.position.y = home_y*0.6;
+  ispy.camera.position.z = home_z*0.6;
+
+  ispy.camera.setZoom(1);
+  ispy.camera.up = new THREE.Vector3(0,1,0);
+  ispy.lookAtOrigin();
+};
 
 ispy.init = function() {
   var display = document.getElementById('display');
@@ -46,9 +67,12 @@ ispy.init = function() {
   var height = $('#display').innerHeight();
 
   // width, height, fov, near, far, orthoNear, orthoFar
-  var camera = new THREE.CombinedCamera(width, height, 70, 1, 100, 1, 100);
+  var camera = new THREE.CombinedCamera(width, height, 75, 0.1, 100, 0.1, 100);
   ispy.camera = camera;
   ispy.initCamera();
+
+  ispy.velocity = new THREE.Vector3(0, 0, 0);
+  ispy.acceleration = new THREE.Vector3(0, 0, 0);
 
   var inset_scene = new THREE.Scene();
   ispy.inset_scene = inset_scene;
@@ -92,18 +116,44 @@ ispy.init = function() {
 
   ispy.stats = new Stats();
   display.appendChild(ispy.stats.domElement);
+
+  // On page load hide the stats
   $('#stats').hide();
-  ispy.show_stats = false;
+  // FF keeps the check state on reload so force an "uncheck"
+  $('#show-stats').prop('checked', false);
+
+  $('#show-stats').change(function() {
+    if ( this.checked ) { // if checked then show
+      $('#stats').show();
+    } else {
+      $('#stats').hide();
+    }
+  });
 
   ispy.inverted_colors = false;
+  $('#invert-colors').prop('checked', false);
 
-  // Make axes and labels XYZ -> RGB
-  var axes = new THREE.AxisHelper(4);
-  axes.material.linewidth = 5;
-  ispy.inset_scene.add(axes);
-  ispy.show_axes = true;
+  var origin = new THREE.Vector3(0,0,0);
+  var rx = new THREE.ArrowHelper(new THREE.Vector3(4,0,0), origin, 4, 0xff0000, 0.01, 0.01);
+  var gy = new THREE.ArrowHelper(new THREE.Vector3(0,4,0), origin, 4, 0x00ff00, 0.01, 0.01);
+  var bz = new THREE.ArrowHelper(new THREE.Vector3(0,0,4), origin, 4, 0x0000ff, 0.01, 0.01);
 
-  //console.log(THREE.FontUtils);
+  rx.line.material.linewidth = 5;
+  gy.line.material.linewidth = 5;
+  bz.line.material.linewidth = 5;
+
+  ispy.inset_scene.add(rx);
+  ispy.inset_scene.add(gy);
+  ispy.inset_scene.add(bz);
+
+  $('#show-axes').prop('checked', false); // FF keeps the state after a page refresh. Therefore force uncheck.
+  $('#show-axes').change(function() {
+    if ( this.checked ) { // if checked then hide axes
+      $('#axes').hide();
+    } else {
+      $('#axes').show();
+    }
+  });
 
   var x_geo = new THREE.TextGeometry('X', {size:0.75, height:0.1});
   var x_color = new THREE.Color(0xff0000);
@@ -137,7 +187,7 @@ ispy.init = function() {
     var obj_group = new THREE.Object3D();
     obj_group.name = g;
     ispy.scene.add(obj_group);
-  })
+  });
 
   $('#version').html("v"+ispy.version);
 
@@ -152,18 +202,59 @@ ispy.init = function() {
   ispy.image_data = null;
 
   ispy.raycaster = new THREE.Raycaster();
-  ispy.raycaster.linePrecision = 0.01;
+  ispy.raycaster.linePrecision = 0.1; // Previously 0.01, but choosing the object was difficult
 
   ispy.mouse = new THREE.Vector2();
   ispy.intersected = null;
 
   ispy.renderer.domElement.addEventListener('mousemove', ispy.onMouseMove, false);
   ispy.renderer.domElement.addEventListener('mousedown', ispy.onMouseDown, false);
-}
+
+  // Are we running an animation?
+  ispy.animating = false;
+
+  ispy.setFramerate(30);
+  $('#fps-slider').prop('value', ispy.framerate); // for FF
+
+  ispy.importTransparency = 1.0;
+  $('#transparency-slider').prop('value', ispy.importTransparency);
+  $('#trspy').html(ispy.importTransparency);
+
+  // Info dialogs are hidden by default (see ispy.css)
+  // FF keeps state on reload so force here
+  $('#show-info').prop('checked', true);
+
+  $('#show-info').change(function() {
+    if ( this.checked ) { // if checked then already visible, so turn off
+      $('.info').css('visibility', 'visible');
+    } else {
+      $('.info').css('visibility', 'hidden');
+    }
+  });
+
+  ispy.stereo = false;
+
+  $('#display').append($('#event-info'));
+};
+
+
+ispy.initLight = function() {
+  var intensity = 1.0;
+  var length = 15.0;
+
+  ispy.light1 = new THREE.DirectionalLight(0xffffff, intensity);
+  ispy.light1.position.set(-length, length, length);
+  ispy.scene.getObjectByName("Detector").add(ispy.light1);
+
+  ispy.light2 = new THREE.DirectionalLight(0xffffff, intensity);
+  ispy.light2.position.set(length, -length, -length);
+  ispy.scene.getObjectByName("Detector").add(ispy.light2);
+};
+
 
 ispy.getScript = function(scr) {
   return $.ajax({url: scr, dataType: "script", cache: true});
-}
+};
 
 ispy.initDetector = function() {
   // Loading and rendering the actual geometry when WebGL is available
@@ -179,8 +270,8 @@ ispy.initDetector = function() {
 
     $('#loading').modal('show');
 
-    $.when(ispy.getScript("./geometry/eb.js"),
-           ispy.getScript("./geometry/ee.js"),
+    $.when(ispy.getScript("./geometry/eb-reduced.min.js"),
+           ispy.getScript("./geometry/ee-reduced.min.js"),
            ispy.getScript("./geometry/hb.js"),
            ispy.getScript("./geometry/ho.js"),
            ispy.getScript("./geometry/hehf.js"),
@@ -188,10 +279,64 @@ ispy.initDetector = function() {
            ispy.getScript("./geometry/tib.js"),
            ispy.getScript("./geometry/tob.js"),
            ispy.getScript("./geometry/tec.js"),
-           ispy.getScript("./geometry/tid.js"))
-           .done(function() {
-            $('#loading').modal('hide');
-            ispy.addDetector();
-          });
+           ispy.getScript("./geometry/tid.js")
+    ).done(function() {
+        $.when(
+          ispy.addDetector()
+        ).done(function() {
+          $('#loading').modal('hide');
+        });
+    });
   }
-}
+};
+
+ispy.render = function() {
+  if ( ispy.renderer !== null ) {
+    if ( ispy.stereo ) {
+      ispy.stereo_renderer.render(ispy.scene, ispy.camera);
+    } else {
+      ispy.renderer.render(ispy.scene, ispy.camera);
+    }
+
+    if ( ispy.get_image_data ){
+      ispy.image_data = ispy.renderer.domElement.toDataURL();
+      ispy.get_image_data = false;
+    }
+  }
+
+  if ( ispy.inset_renderer !== null ) {
+    ispy.inset_renderer.render(ispy.inset_scene, ispy.inset_camera);
+  }
+};
+
+ispy.run = function() {
+  setTimeout( function() {
+    requestAnimationFrame(ispy.run);
+  }, 1000 / ispy.framerate );
+
+  ispy.stats.update();
+
+  if ( ispy.stereo ) {
+    ispy.do_controls.update();
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    ispy.camera.aspect = width / height;
+    ispy.camera.updateProjectionMatrix();
+    ispy.stereo_renderer.setSize(width, height);
+  } else {
+    ispy.controls.update();
+    ispy.inset_camera.position.subVectors(ispy.camera.position, ispy.controls.target);
+  }
+
+  ispy.inset_camera.up = ispy.camera.up;
+  ispy.inset_camera.quarternion = ispy.camera.quaternion;
+  ispy.inset_camera.position.setLength(10);
+  ispy.inset_camera.lookAt(ispy.inset_scene.position);
+
+  ispy.render();
+
+  if ( ispy.animating ) {
+    TWEEN.update();
+  }
+};
