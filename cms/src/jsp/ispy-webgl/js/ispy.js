7613063089329af4ef85da0a61c2af71aeb1c9ec
eperonja <edit.peronja@gmail.com>
2016-01-19 17:19:28 +0000

    Ispy-webgl: update by Tom

---
+++ cms/src/jsp/ispy-webgl/js/ispy.js
@@ -0,0 +1,12 @@
+
+$(function() {
+  ispy.init();
+  ispy.addGroups();
+  ispy.initLight();
+  ispy.initDetector();
+  //ispy.importBeampipe();
+  ispy.loadWebFiles();
+  ispy.run();
+
+  console.log(ispy.event_description);
+});
