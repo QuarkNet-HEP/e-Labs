217f03a2397d554c29bd048fc939284d92ed2dda
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2016-03-24 14:51:53 +0000

    test change to JavaMail smtp host anl->fnal

--- common/src/java/gov/fnal/elab/usermanagement/impl/DatabaseUserManagementProvider.java
+++ common/src/java/gov/fnal/elab/usermanagement/impl/DatabaseUserManagementProvider.java
@@ -1179,43 +1179,53 @@ public class DatabaseUserManagementProvider implements
     }//end of get username from email address
 
     //EPeronja: send e-mail to users
+    /// JavaMail: see https://en.wikipedia.org/wiki/JavaMail for structure
     public String sendEmail(String to, String subject, String message) throws ElabException {
     	String result = "";
-		//Sender's email ID 
-		final String from = elab.getProperty("retrieveResetEmail");
-		final String password = "";
-	    //Get system properties object
-	    Properties properties = System.getProperties();
-	    //Setup mail server
-	    properties.put("mail.smtp.host", "smtp.mcs.anl.gov");
-	    properties.put("mail.smtp.port", "25");
-	    properties.put("mail.smtp.auth", "true");
-	    properties.put("mail.smtp.starttls.enable", "true");			    
-	    //Get the default Session object.
-	    //Session mailSession = Session.getDefaultInstance(properties);
-	   	Session mailSession = Session.getInstance(properties, new javax.mail.Authenticator() {
-	   		protected PasswordAuthentication getPasswordAuthentication() {
-	   			return new PasswordAuthentication(from, password );
-	   		}
-	   	});
-	    try{
-	       //Create a default MimeMessage object.
-	       MimeMessage msg = new MimeMessage(mailSession);
-	       //Set From: header field of the header.
-	       msg.setFrom(new InternetAddress(from));
-	       //Set To: header field of the header.
-	       msg.addRecipient(Message.RecipientType.TO,
-	                               new InternetAddress(to));
-	       // Set Subject: header field
-	       msg.setSubject(subject);
-	       msg.setText(message);
-	       //Send message
-	       Transport.send(msg); 
-		} catch (MessagingException mex) {
-		      mex.printStackTrace();
-		      result = "Error: unable to send message. " + mex.toString();
-		}	
-	    return result;
+
+	//Sender's email ID 
+	final String from = elab.getProperty("retrieveResetEmail");
+	final String password = "";
+
+	//Get system properties object
+	Properties properties = System.getProperties();
+
+	//Setup mail server
+	// If using static Transport.send() (we are), need to specify which host to send it to
+	//properties.put("mail.smtp.host", "smtp.mcs.anl.gov");
+	properties.put("mail.smtp.host", "smtp.fnal.gov");
+	properties.put("mail.smtp.port", "25");
+	properties.put("mail.smtp.auth", "true");
+	properties.put("mail.smtp.starttls.enable", "true");			    
+
+	//Get the default Session object.
+	//Session mailSession = Session.getDefaultInstance(properties);
+	/// Parentheses look weird, but are correct:
+	/// Session mailSession = Session.getDefaultInstance(properties, big new thing {} );
+	Session mailSession = Session.getInstance(properties, new javax.mail.Authenticator() {
+		protected PasswordAuthentication getPasswordAuthentication() {
+		    return new PasswordAuthentication(from, password );
+		}
+	});
+
+	try{
+	    //Create a default MimeMessage object.
+	    MimeMessage msg = new MimeMessage(mailSession);
+	    //Set From: header field of the header.
+	    msg.setFrom(new InternetAddress(from));
+	    //Set To: header field of the header.
+	    msg.addRecipient(Message.RecipientType.TO,
+			     new InternetAddress(to));
+	    // Set Subject: header field
+	    msg.setSubject(subject);
+	    msg.setText(message);
+	    //Send message
+	    Transport.send(msg); 
+	} catch (MessagingException mex) {
+	    mex.printStackTrace();
+	    result = "Error: unable to send message. " + mex.toString();
+	}	
+	return result;
     }//end of sendEmail
     
     //EPeronja: update active/inactive status 
