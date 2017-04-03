1b20e899e4f348b3ae038034a0bda203626d1cc7
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2017-04-03 16:31:26 +0000

    Debugging Delta-t upgrades

--- cosmic/src/java/gov/fnal/elab/cosmic/EventCandidates.java
+++ cosmic/src/java/gov/fnal/elab/cosmic/EventCandidates.java
@@ -324,6 +324,8 @@ public class EventCandidates {
 						// check ids[] for Dt conditions
 						if ((ids.size() > 1) && (ids.get(0) != null) && (ids.get(1) != null)) {
 						 ////			Integer[] dets = {Integer.parseInt(ids.get(0)), Integer.parseInt(ids.get(1))};
+								String det1 = ids.get(0);
+								String det1 = ids.get(1);
 								dets[0] = Integer.parseInt("1");
 								dets[1] = Integer.parseInt("1");
 						////		dets[0] = 0;
