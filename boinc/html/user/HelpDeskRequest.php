a37a3ed9bf3a3c3353f78d421f3d29259bbd501d
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2018-03-27 21:47:49 +0000

    Reverted CAPTCHA removal

--- boinc/html/user/HelpDeskRequest.php
+++ boinc/html/user/HelpDeskRequest.php
@@ -136,38 +136,38 @@ if( $_SERVER["REMOTE_ADDR"] == "198.129.208.188" ){
  * We only present a reCAPTCHA for users who are not already logged in.
  */
 
-////require_once("../include/recaptchalib.php");
+require_once("../include/recaptchalib.php");
 
 // The keys are kept in these separate files instead of
 // in the source code because the source code may be publicly
 // available via SVN or CVS.  Please keep it that way!
 // These are (for now) the Spy Hill keys.
 //
-////$pub_key_file = "../../keys/reCAPTCHA_public_key";
-////$priv_key_file = "../../keys/reCAPTCHA_private_key";
-////$mailhide_pub_key_file  = "../../keys/reCAPTCHA_public_key";
-////$mailhide_priv_key_file = "../../keys/reCAPTCHA_private_key";
+$pub_key_file = "../../keys/reCAPTCHA_public_key";
+$priv_key_file = "../../keys/reCAPTCHA_private_key";
+$mailhide_pub_key_file  = "../../keys/reCAPTCHA_public_key";
+$mailhide_priv_key_file = "../../keys/reCAPTCHA_private_key";
 
 // Verify the keys exist and are usable
 //
-////if( !file_exists($pub_key_file) || !file_exists($priv_key_file) ||
-////    !file_exists($mailhide_priv_key_file) || !file_exists($mailhide_pub_key_file) ) {
-////    error_page("Server configuration error. Cannot access keys.
-////        Please report this to the project administrators.");
-////}
+if( !file_exists($pub_key_file) || !file_exists($priv_key_file) ||
+    !file_exists($mailhide_priv_key_file) || !file_exists($mailhide_pub_key_file) ) {
+    error_page("Server configuration error. Cannot access keys.
+        Please report this to the project administrators.");
+}
 
-////$public_key = file_get_contents($pub_key_file);
-////$private_key = file_get_contents($priv_key_file);
-////$mailhide_public_key = trim(file_get_contents($mailhide_pub_key_file));
-////$mailhide_private_key = trim(file_get_contents($mailhide_priv_key_file));
+$public_key = file_get_contents($pub_key_file);
+$private_key = file_get_contents($priv_key_file);
+$mailhide_public_key = trim(file_get_contents($mailhide_pub_key_file));
+$mailhide_private_key = trim(file_get_contents($mailhide_priv_key_file));
 
-////if( empty($public_key) || empty($private_key) ){
-////    error_page("Server configuration error. Empty key.
-////        Please report this to the project administrators.");
-////}
+if( empty($public_key) || empty($private_key) ){
+    error_page("Server configuration error. Empty key.
+        Please report this to the project administrators.");
+}
 
 // Used by the reCAPTCHA PHP API to enforce secure requests
-////$use_ssl = true;
+$use_ssl = true;
 /*******************************
  * Local functions:
  *   (some of these will move to ../include/util.php when finished)
@@ -306,12 +306,12 @@ function error_text($name){
         $text="Please supply a VALID e-mail address.";
         break;
 
-////    case 'recaptcha':
-////        $text="Incorrect answer.<br/>Please try again.";
-////        break;
-////    case 'noverify':
-////        $text="Please enter an answer. ";
-////        break;
+    case 'recaptcha':
+        $text="Incorrect answer.<br/>Please try again.";
+        break;
+    case 'noverify':
+        $text="Please enter an answer. ";
+        break;
     }
 
     // It's not an error if it wasn't found above
@@ -684,7 +684,7 @@ function do_post($url, $data)
     global $subject, $problem, $error_msg;
     global $elab, $elab_list, $elab_forum_id, $forum_id;
     global $user_name, $user_role, $role_list, $return_address;
-////    global $mailhide_public_key, $mailhide_private_key;
+    global $mailhide_public_key, $mailhide_private_key;
 
     if( !array_key_exists($elab,$elab_forum_id) ) {
       debug_msg(1,"Cannot find forum_id for e-Lab $elab");
@@ -948,24 +948,24 @@ if( isset($_POST['submit_report']) && empty($input_error) ){
 
     // If the person is not logged in then we need to check the CAPTCHA
     //
-////    if( !$logged_in_user ){
-////        if( empty($_POST["recaptcha_response_field"]) ){
-////            $input_error['noverify']++;
-////        }
-////        else {
-////            $resp = recaptcha_check_answer ($private_key,
-////                                            $_SERVER["REMOTE_ADDR"],
-////                                            $_POST["recaptcha_challenge_field"],
-////                                            $_POST["recaptcha_response_field"]);
-////
-////            if( !$resp->is_valid ) $input_error['recaptcha']++;
-////        }
-////    }
+    if( !$logged_in_user ){
+        if( empty($_POST["recaptcha_response_field"]) ){
+            $input_error['noverify']++;
+        }
+        else {
+            $resp = recaptcha_check_answer ($private_key,
+                                            $_SERVER["REMOTE_ADDR"],
+                                            $_POST["recaptcha_challenge_field"],
+                                            $_POST["recaptcha_response_field"]);
+
+            if( !$resp->is_valid ) $input_error['recaptcha']++;
+        }
+    }
 
 
     // Sumbit via e-mail and forum post
     //
-////    if( empty($input_error) ){
+    if( empty($input_error) ){
 
         page_head("Problem report submission");
 
@@ -1017,7 +1017,7 @@ if( isset($_POST['submit_report']) && empty($input_error) ){
 
         page_tail();
         exit;
-    ////}
+    }
  }
 
 
@@ -1254,13 +1254,13 @@ if( !$logged_in_user ) {
               "<input name='location' value='$location'
                         size='60' maxlength='255'>");
 
-////    form_item("Verification:",
-////              "Please enter the two words shown in the box, to prove
-////                that you are a human, not an automated web-bot."
-////              .error_text('noverify')
-////              .error_text('recaptcha'),
-////              recaptcha_get_html($public_key, NULL, $use_ssl));
-//// }
+    form_item("Verification:",
+              "Please enter the two words shown in the box, to prove
+                that you are a human, not an automated web-bot."
+              .error_text('noverify')
+              .error_text('recaptcha'),
+              recaptcha_get_html($public_key, NULL, $use_ssl));
+ }
 
 form_item("Send the report:", "",
      "<input name='submit_report' type='SUBMIT' value='Submit'>");
