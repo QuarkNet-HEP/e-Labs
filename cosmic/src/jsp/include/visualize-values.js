b70638d54a87e5bbb2c629ced029d06f21088246
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2019-09-09 20:55:20 +0000

    Moved coordinate delcarations out of if-branches

--- cosmic/src/jsp/include/visualize-values.js
+++ cosmic/src/jsp/include/visualize-values.js
@@ -30,48 +30,50 @@ function HideShowChannel(ID) {
 }
 
 //Note:  For a new geometry, all channels are by default inactive and initialized to x=y=z=0.
-function VisualizeValues(t1, t2, t3, t4)
-{
-if (t1 == true){
-var x1 = document.getElementById("myForm").elements.namedItem("chan1X").value;
-var y1 = document.getElementById("myForm").elements.namedItem("chan1Y").value;
-var z1 = document.getElementById("myForm").elements.namedItem("chan1Z").value;
-}
-else{
-	x1 = 0; y1 = 0; z1 = 0;
-}
+function VisualizeValues(t1, t2, t3, t4){
+		let x1,y1,z1;
+		let x2,y2,z2;
+		let x3,y3,z3;
+		let x4,y4,z4;
 
-if (t2 == true){
-var x2 = document.getElementById("myForm").elements.namedItem("chan2X").value;
-var y2 = document.getElementById("myForm").elements.namedItem("chan2Y").value;
-var z2 = document.getElementById("myForm").elements.namedItem("chan2Z").value;
-}
-else{
-	x2 = 0; y2 = 0; z2 = 0;
-}
+		if (t1 == true){
+				x1 = document.getElementById("myForm").elements.namedItem("chan1X").value;
+				y1 = document.getElementById("myForm").elements.namedItem("chan1Y").value;
+				z1 = document.getElementById("myForm").elements.namedItem("chan1Z").value;
+		} else{
+				x1 = 0; y1 = 0; z1 = 0;
+		}
 
-if (t3 == true){
-var x3 = document.getElementById("myForm").elements.namedItem("chan3X").value;
-var y3 = document.getElementById("myForm").elements.namedItem("chan3Y").value;
-var z3 = document.getElementById("myForm").elements.namedItem("chan3Z").value;
-}
-else{
-	x3 = 0; y3 = 0; z3 = 0;
-}
+		if (t2 == true){
+				x2 = document.getElementById("myForm").elements.namedItem("chan2X").value;
+				y2 = document.getElementById("myForm").elements.namedItem("chan2Y").value;
+				z2 = document.getElementById("myForm").elements.namedItem("chan2Z").value;
+		} else{
+				x2 = 0; y2 = 0; z2 = 0;
+		}
 
-if (t4 == true){
-var x4 = document.getElementById("myForm").elements.namedItem("chan4X").value;
-var y4 = document.getElementById("myForm").elements.namedItem("chan4Y").value;
-var z4 = document.getElementById("myForm").elements.namedItem("chan4Z").value;
-}
-else{
-	x4 = 0; y4 = 0; z4 = 0;
-}	
+		if (t3 == true){
+				x3 = document.getElementById("myForm").elements.namedItem("chan3X").value;
+				y3 = document.getElementById("myForm").elements.namedItem("chan3Y").value;
+				z3 = document.getElementById("myForm").elements.namedItem("chan3Z").value;
+		}
+		else{
+				x3 = 0; y3 = 0; z3 = 0;
+		}
 
-var urlwithcoord = "visualize.jsp?x1="+x1+"&y1="+y1+"&z1="+z1;
-urlwithcoord = urlwithcoord+"&x2="+x2+"&y2="+y2+"&z2="+z2;
-urlwithcoord = urlwithcoord+"&x3="+x3+"&y3="+y3+"&z3="+z3;
-urlwithcoord = urlwithcoord+"&x4="+x4+"&y4="+y4+"&z4="+z4;
+		if (t4 == true){
+				x4 = document.getElementById("myForm").elements.namedItem("chan4X").value;
+				y4 = document.getElementById("myForm").elements.namedItem("chan4Y").value;
+				z4 = document.getElementById("myForm").elements.namedItem("chan4Z").value;
+		}
+		else{
+				x4 = 0; y4 = 0; z4 = 0;
+		}	
 
-window.open (urlwithcoord,"mywindow");
+		let urlwithcoord = "visualize.jsp?x1="+x1+"&y1="+y1+"&z1="+z1;
+		urlwithcoord = urlwithcoord+"&x2="+x2+"&y2="+y2+"&z2="+z2;
+		urlwithcoord = urlwithcoord+"&x3="+x3+"&y3="+y3+"&z3="+z3;
+		urlwithcoord = urlwithcoord+"&x4="+x4+"&y4="+y4+"&z4="+z4;
+
+		window.open (urlwithcoord,"mywindow");
 }
