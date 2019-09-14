d999da3dad70c3d9b4107ca50d14eb29691c237c
eperonja <edit.peronja@gmail.com>
2019-09-14 15:00:00 +0000

    ThresholdTimes update to time calculation

--- cosmic/src/java/gov/fnal/elab/cosmic/analysis/ThresholdTimes.java
+++ cosmic/src/java/gov/fnal/elab/cosmic/analysis/ThresholdTimes.java
@@ -267,13 +267,18 @@ public class ThresholdTimes {
                     if (newtempdiff == tempdiff && tempdiff < -0.9 && retime[channel] < 0.1) {
                 		jd = currLineJD(offset, parts) + 1;
                     } else {                    	
-                        jd = currLineJD(offset, parts);           		            	
+                        jd = currLineJD(offset, parts);
+                        //this is to prevent rolling over too soon
+                        tempdiff = jd+retime[channel] - lastjdplustime;
+                        if (lastjdplustime > 0 && tempdiff >= 1.0) {
+                        	jd = jd-1;
+                        }
                     }
             	} 
             } else {
                 jd = currLineJD(offset, parts);           		            	
             }
-
+            
             lastGPSDay = currGPSDay;
             lastEdgeTime = retime[channel];
         }
