539122fe0331ee5c5b84d030e7bcbcbcdd379251
JG-QuarkNet <16691642+JG-QuarkNet@users.noreply.github.com>
2020-03-25 19:50:03 +0000

    Added comments

--- common/src/java/gov/fnal/elab/RawDataFileResolver.java
+++ common/src/java/gov/fnal/elab/RawDataFileResolver.java
@@ -6,6 +6,9 @@ package gov.fnal.elab;
 
 import java.io.File;
 
+/* This class takes a filename and an e-Lab and returns the absolute path 
+ * to the given file with respect to the e-Lab's data directory - JG 25Mar2020
+ */
 public abstract class RawDataFileResolver {
     private static RawDataFileResolver def;
 
@@ -20,21 +23,35 @@ public abstract class RawDataFileResolver {
         def = resolver;
     }
 
+		/* 'lfn' represents the "logical filename"
+		 * We expect it to be of the form 
+		 *   6119.2019.0730.0
+		 * We want to return the full path to this file.
+		 * 'dataDir' is the absolute path of the e-Lab's data directory;
+		 * it will form the basis of the returned filepath.
+		 */
+		/* These three methods account for different input signatures */
     public abstract String resolve(Elab elab, String lfn);
-    
+
     public abstract String resolve(String dataDir, String lfn);
-    
+
     public abstract String[] resolveAll(Elab elab, String[] lfns);
 
     public static class Default extends RawDataFileResolver {
         public String resolve(Elab elab, String lfn) {
             return resolve(elab.getProperties().getDataDir(), lfn);
         }
-        
+
+				/* This method does all the work */
         public String resolve(String dataDir, String lfn) {
+						/* Find the first 'period'.  If it doesn't exist, return the
+						 * filepath as `dataDir/lfn`
+						 * If it does exist, extract the DAQ as the first segment and 
+						 * return the filepath as `dataDir/DAQ/lfn
+						 */
             int i = lfn.indexOf('.');
             if (i == -1) {
-                return dataDir + File.separator + lfn; 
+                return dataDir + File.separator + lfn;
             }
             else {
                 String detectorid = lfn.substring(0, i);
@@ -43,6 +60,7 @@ public abstract class RawDataFileResolver {
             }
         }
 
+				/* This method is the same as resolve() but with Array in -> Array out */
         public String[] resolveAll(Elab elab, String[] lfns) {
             String[] r = new String[lfns.length];
             String dataDir = elab.getProperties().getDataDir();
