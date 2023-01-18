9d33b1a7309001755f5649eaa6bfd07a47993c08
Edit Peronja <edit.peronja@gmail.com>
2023-01-18 14:55:50 -0700

    Updated Rate vs Pressure datetime

--- cosmic/src/jsp/analysis-blessing/rate-pressure.js
+++ cosmic/src/jsp/analysis-blessing/rate-pressure.js
@@ -21,16 +21,17 @@ var medianKey, medianError;
 
 Date.prototype.customFormat = function(formatString){
     var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+    
     var dateObject = this;
     YY = ((YYYY=dateObject.getFullYear())+"").slice(-2);
     MM = (M=dateObject.getMonth()+1)<10?('0'+M):M;
     MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
-    DD = (D=dateObject.getDate())<10?('0'+D):D;
+    DD = (D=dateObject.getDate()-1)<10?('0'+D):D;
     DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObject.getDay()]).substring(0,3);
     th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
     formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
 
-    h=(hhh=dateObject.getHours());
+    h=(hhh=dateObject.getUTCHours());
     if (h==0) h=24;
     if (h>12) h-=12;
     hh = h<10?('0'+h):h;
@@ -171,18 +172,18 @@ options = {
 			mode: "time",
 			tickFormatter: function (val, axis) {		
 				var d = new Date(val);
-				return d.customFormat("#DD#/#MMM#<br />#hh#:#mm#");
+				return d.customFormat("#DD#/#MMM#<br />#hh#:#mm##AMPM#");
 				}
-			}, {	
+			}, 
+			{	
 			axisLabelUseCanvas: true,
 			mode: "time",
 			tickFormatter: function (val, axis) {		
 				var d = new Date(val);
-				return d.customFormat("#DD#/#MMM#<br />#hh#:#mm#");
+				return d.customFormat("#DD#/#MMM#<br />#hh#:#mm##AMPM#");
 				}
 			}
-	]
-		
+			],	
 	};
 
 optionsExtra = {
