9f4670f116c3a5996508fd84a8fe55a9db378f9e
Edit Peronja <edit.peronja@gmail.com>
2026-01-14 08:52:03 -0700

    updates to the pyramid project

--- cosmic/src/jsp/pyramid-display/js/main.js
+++ cosmic/src/jsp/pyramid-display/js/main.js
@@ -113,7 +113,6 @@ function getSingleGeometry() {
 	});	
 	//console.log(layerOrderY);
 }// end of getGeometry
-const loadingMessage = document.getElementById("loading-message");
 
 // Load the selected data file
 function loadDataFile() {
